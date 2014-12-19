48b9081bf377f96cff8ae788daa3cadb8e672538
Jan Böcker <jan.boecker@jboecker.de>
2014-12-19 12:22:24 +0100

    move documentation to DCS-BIOS\Scripts\doc

--- Scripts/DCS-BIOS/lib/Protocol.lua
+++ Scripts/DCS-BIOS/lib/Protocol.lua
@@ -29,13 +29,13 @@ end
 function BIOS.protocol.endModule()
 	local function saveDoc()
 		local JSON = loadfile([[Scripts\JSON.lua]])()
-		local file, err = io.open(lfs.writedir()..[[Scripts\DCS-BIOS\doc\]]..moduleBeingDefined.name..".json", "w")
+		local file, err = io.open(lfs.writedir()..[[Scripts\DCS-BIOS\doc\json\]]..moduleBeingDefined.name..".json", "w")
 		local json_string = JSON:encode_pretty(moduleBeingDefined.documentation)
 		if file then
 			file:write(json_string)
 			file:close()
 		end
-		local file, err = io.open(lfs.writedir()..[[Scripts\DCS-BIOS\doc\]]..moduleBeingDefined.name..".jsonp", "w")
+		local file, err = io.open(lfs.writedir()..[[Scripts\DCS-BIOS\doc\json\]]..moduleBeingDefined.name..".jsonp", "w")
 		if file then
 			file:write('docdata["'..moduleBeingDefined.name..'"] =\n')
 			file:write(json_string)
