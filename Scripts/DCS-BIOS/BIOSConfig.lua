71dae748c93ba9c8ec32ac1ac9bae25a9acf4b38
Jan Böcker <jan.boecker@jboecker.de>
2014-12-20 19:28:57 +0100

    add TCP support

--- Scripts/DCS-BIOS/BIOSConfig.lua
+++ Scripts/DCS-BIOS/BIOSConfig.lua
@@ -1,5 +1,6 @@
 BIOS.protocol_io.connections = {
 	BIOS.protocol_io.DefaultMulticastSender:create(),
+	BIOS.protocol_io.TCPServer:create(),
 	-- BIOS.protocol_io.UDPSender:create({ port = 7777, host = "192.168.1.177" }),
 	BIOS.protocol_io.UDPListener:create({ port = 7778 })
 }
--- Scripts/DCS-BIOS/lib/ProtocolIO.lua
+++ Scripts/DCS-BIOS/lib/ProtocolIO.lua
@@ -13,6 +13,77 @@ function BIOS.protocol_io.LuaSocketConnection:close()
 	self.conn:close()
 end
 
+BIOS.protocol_io.TCPServer = {}
+function BIOS.protocol_io.TCPServer:create(args)
+	args = args or {}
+	local self = BIOS.protocol_io.LuaSocketConnection:create()
+	BIOS.util.shallowCopy(BIOS.protocol_io.TCPServer, self)
+	self.host = args.host or "*"
+	self.port = args.port or 7778
+	return self
+end
+function BIOS.protocol_io.TCPServer:init()
+	self.acceptor = socket.bind(self.host, self.port, 10)
+	self.acceptor:settimeout(0)
+	self.connections = {}
+end
+function BIOS.protocol_io.TCPServer:step()
+	-- accept new connections
+	local newconn = self.acceptor:accept()
+	if newconn then
+		newconn:settimeout(0)
+		local newconn_info = { conn = newconn, txbuf = "", rxbuf = "" }
+		self.connections[#self.connections+1] = newconn_info
+	end
+
+	local have_closed_connections = false
+	-- receive data
+	for _, conninfo in pairs(self.connections) do
+	
+		local data, err, partial = conninfo.conn:receive(4096)
+		if data then
+			conninfo.rxbuf = conninfo.rxbuf .. data
+		elseif partial and #partial > 0 then
+			conninfo.rxbuf = conninfo.rxbuf .. partial
+		elseif err == "closed" then
+			conninfo.closed = true
+			have_closed_connections = true
+		end
+	
+		while true do
+			local line, rest = conninfo.rxbuf:match("^([^\n]*)\n(.*)")
+			if line then
+				conninfo.rxbuf = rest
+				BIOS.protocol.processInputLine(line)
+			else
+				break
+			end
+		end
+	end
+	
+	-- eliminate closed connections
+	if have_closed_connections then
+		local old_connections = self.connections
+		self.connections = {}
+		for _, conninfo in pairs(old_connections) do
+			if not conninfo.closed then
+				self.connections[#self.connections+1] = conninfo
+			end
+		end
+	end
+end
+function BIOS.protocol_io.TCPServer:send(msg)
+	for _, conninfo in pairs(self.connections) do
+		socket.try(conninfo.conn:send(msg))
+	end
+end
+function BIOS.protocol_io.TCPServer:close()
+	for _, conninfo in pairs(self.connections) do
+		socket.try(conninfo.conn:close())
+	end
+	self.connections = {}
+end
+
 BIOS.protocol_io.DefaultMulticastSender = {}
 function BIOS.protocol_io.DefaultMulticastSender:create()
 	local self = BIOS.protocol_io.LuaSocketConnection:create()
