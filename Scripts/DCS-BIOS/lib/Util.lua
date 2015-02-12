d55a40008cf31003c170cdb5e730b8f304977915
Jan Böcker <jan.boecker@jboecker.de>
2015-02-12 12:47:29 +0100

    defineIndicatorLight: treat all values != 0 as "on" (fixes #45)

--- Scripts/DCS-BIOS/lib/Util.lua
+++ Scripts/DCS-BIOS/lib/Util.lua
@@ -276,7 +276,11 @@ function BIOS.util.defineIndicatorLight(msg, arg_number, category, description)
 	}
 	assert(value.shiftBy ~= nil)
 	moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function(dev0)
-		value:setValue(dev0:get_argument_value(arg_number))
+		if dev0:get_argument_value(arg_number) == 0 then
+			value:setValue(0)
+		else
+			value:setValue(1)
+		end
 	end
 	document {
 		identifier = msg,
