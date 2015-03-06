10259a081e1ead4794b19309bc1c20c1faca7ca0
wraith444 <fourthwingman@gmail.com>
2015-03-05 22:36:28 -0700

    MiG-21: 131 clickable controls added
    Center Lower:
    	Emergency nose gear handle
    	Altitude warning set knob
    	Tactical drop cover
    	Tactical drop switch
    	Emergency missile launch cover
    	Emergency missile launch button
    
    Center Mid:
    	RADAR display magnetic reset button
    	RADAR continuous interrupt button
    	RADAR temporary interrupt button
    	RADAR passive interrupt button
    	RADAR weather interrupt button
    	RADAR low speed interrupt button
    	RADAR self test button
    	RADAR display polar filter slider
    	SPRD start cover
    	SPRD start button
    	Emergency brake switch
    	KPP cage button
    	KPP pitch knob
    	NPP adjust button
    	NPP course set knob
    	Altimeter pressure knob
    	Wing drop tank jettison cover
    	Wing drop tank jettison button
    	Outboard jettison cover
    	Outboard jettison button
    	Inboard jettison cover
    	Inboard jettison button
    
    Center Upper:
    	SPO volume knob
    	SPO test button
    	SPO brightness adjustment
    	Nosegear brake switch
    	Nosecone adjustment knob
    	Close canopy button
    	Canopy anti-ice button
    	AA/AG mode switch
    	IR/neutral/SAR mode switch
    	Gun pyro charge 1 button
    	Gun pyro charge 2 button
    	Gun pyro charge 3 button
    	Weapon selection knob
    
    Left Horizontal:
    	SPRD drop cover
    	SPRD drop button
    	Airbrake button
    	Neutral flaps button
    	Takeoff flaps button
    	Landing flaps button
    	Flaps reset button
    	Drogue chute release cover
    	Drogue chute release button
    	Aileron booster switch
    	Helmet air conditioning switch
    	Emergency oxygen switch
    	Oxygen mixture switch
    
    Left Vertical:
    	SAU stabilizing mode button
    	SAU low altitude switch
    	SAU command landing button
    	SAU automatic landing button
    	SAU reset button
    	SPS power switch
    	ARU auto/manual switch
    	ARU low/high speed switch
    	ABS switch
    	Gear handle lock
    	Gear handle
    	Drogue chute deploy button
    	Nosecone auto/manual toggle
    	Nozzle position toggle
    	Canopy lock lever
    	Canopy pressurize lever
    	Open canopy button
    	TDC range/pipper span twist grip
    	IR seeker volume knob
    	Helmet heat auto/manual switch
    	Helmet quick heat button
    	Helmet visor toggle
    	Air conditioning toggle
    
    Right Horizontal:
    	SPRD power switch
    	SPRD drop mode power switch
    
    Right Vertical:
    	SAU power switch
    	SAU pitch power switch
    	SPO power switch
    	SRZO power switch
    	SRZO code select knob
    	SRZO type 81 power switch
    	SRZO emergency transmit cover
    	SRZO emergency transmit switch
    	SRZO self destruct cover
    	SRZO self destruct button
    	SOD power switch
    	SOD identify button
    	SOD wave select switch
    	SOD mode select switch
    	RADAR standby/power switch
    	RADAR low alt mode switch
    	RADAR fixed beam switch
    	Emergency gear release lever
    	Trimmer power switch
    	Nosecone power switch
    	Emergency main hydraulics pump switch
    	KPP main/emergency switch
    	NPP power switch
    	Radio altimeter power
    	Canopy emergency jettison lever
    	Canopy ventilation lever
    	Gunsight power switch
    	Missile/rocket heat switch
    	Missile/rocket launch switch
    	Pylons 1-2 power switch
    	Pylons 3-4 power switch
    	Cannon power switch
    	Gun camera power switch
    
    Stick:
    	SAU cancel button
    	SAU recovery button
    	RADAR lock button
    	Trimmer switch
    	Centerline droptank jettison button
    	Gun trigger
    	Weapons release button
    	Weapons release cover
    
    Gunsight:
    	Gunsight auto/manual switch
    	Gunsight shoot/bomb switch
    	Gunsight gun/missile switch
    	Gunsight missile/gyro lever
    	Gunsight pipper on/off switch
    	Gunsight fixed net on/off switch
    	Gunsight target range knob
    	Gunsight intercept angle knob
    	Gunsight scale brightness knob
    	Gunsight pipper brightness knob
    	Gunsight fixed brightness knob

--- Scripts/DCS-BIOS/lib/Mig21.lua
+++ Scripts/DCS-BIOS/lib/Mig21.lua
@@ -119,6 +119,191 @@ defineToggleSwitch("ENG_STOP", devices.ENGINE_START_DEVICE, device_commands.RUDS
 defineToggleSwitch("EMR_AFB_PWR", devices.ENGINE, device_commands.Chr, 320, "Left Horizontal", "Emergency Afterburner")
 definePushButton("RAD_PTT", devices.INTERCOM, device_commands.RadioINTERCOM, 315, "Left Horizontal", "Radio PTT")
 
+-- SAU
+defineToggleSwitch("SAU_PWR", devices.SAU, device_commands.SAUon, 179, "Right Vertical", "SAU On/Off")
+defineToggleSwitch("SAU_PITCH_PWR", devices.SAU, device_commands.SAUpitchOn, 180, "Right Vertical", "SAU Pitch On/Off")
+definePushButton("SAU_STAB", devices.SAU, device_commands.SAUstabil, 343, "Left Vertical", "SAU Stabilization Mode")
+definePushButton("SAU_CANCEL", devices.SAU, device_commands.SAUcancel, 376, "Stick", "SAU Cancel Current Mode")
+definePushButton("SAU_RECOVERY", devices.SAU, device_commands.SAUprivedenie, 377, "Stick", "SAU Recovery Mode")
+defineToggleSwitch("SAU_LOW", devices.SAU, device_commands.SAUlowAltOn, 344, "Left Vertical", "SAU Limit Altitude")
+definePushButton("SAU_LAND_CMD", devices.SAU, device_commands.SAUlandingControlCommand, 341, "Left Vertical", "SAU Command Landing Mode")
+definePushButton("SAU_LAND_AUTO", devices.SAU, device_commands.SAUlandingControlAuto, 342, "Left Vertical", "SAU Auto Landing Mode")
+definePushButton("SAU_RST", devices.SAU, device_commands.SAUresetOff, 348, "Left Vertical", "SAU Reset/Off")
+
+-- SPO
+defineToggleSwitch("SPO_PWR", devices.SPO, device_commands.SPOon, 202, "Right Vertical", "SPO On/Off")
+definePushButton("SPO_TEST", devices.SPO, device_commands.SPOtest, 226, "Center Upper", "SPO Test")
+defineToggleSwitch("SPO_DAY", devices.SPO, device_commands.SPOdayNight, 227, "Center Upper", "SPO Day/Night")
+definePotentiometer("SPO_VOL", devices.SPO, device_commands.SPOvolume, 225, {0, 1}, "Center Upper", "SPO Volume")
+
+-- SRZO
+defineToggleSwitch("SRZO_PWR", devices.SRZO, device_commands.SRZOvopros, 188, "Right Vertical", "SRZO IFF Coder/Decoder On/Off")
+defineTumb("SRZO_CODE", devices.SRZO, device_commands.SRZOcodes, 192, 0.08, {0.0, 0.88}, nil, true, "Right Vertical", "SRZO Codes")
+defineToggleSwitch("SRZO_81_PWR", devices.SRZO, device_commands.SRZOon, 346, "Right Vertical", "SRZO Type 81 On/Off")
+defineToggleSwitch("SRZO_EM_TX_CVR", devices.SRZO, device_commands.EmergTransmitSafetyCover, 190, "Right Vertical", "SRZO Emergency Transmit Cover")
+defineToggleSwitch("SRZO_EM_TX", devices.SRZO, device_commands.EmergTransmitOn, 191, "Right Vertical", "SRZO Emergency Transmit")
+defineToggleSwitch("SRZO_DEST_CVR", devices.SRZO, device_commands.SRZODestrCover, 427, "Right Vertical", "SRZO Self Destruct Cover")
+definePushButton("SRZO_DEST", devices.SRZO, device_commands.SRZODestr, 428, "Right Vertical", "SRZO Self Destruct")
+
+-- SOD (increase radar signal for ATC radar, most likely won't be implemented)
+defineToggleSwitch("SOD_PWR", devices.SOD, device_commands.SODon, 200, "Right Vertical", "SOD IFF On/Off")
+definePushButton("SOD_IDENT", devices.SOD, device_commands.SODident, 199, "Right Vertical", "SOD Identify")
+defineTumb("SOD_WAVE", devices.SOD, device_commands.SODvolni, 201, 0.5, {0, 1}, nil, false, "Right Vertical", "SOD Wave Selector 3/1/2")
+defineTumb("SOD_MODE", devices.SOD, device_commands.SODmode, 204, .25, {0, 1}, nil, false, "Right Vertical", "SOD Mode Select")
+
+--RADAR
+defineTumb("RADAR_PWR", devices.RADAR, device_commands.RADARon, 205, 0.5, {0, 1}, nil, false, "Right Vertical", "RADAR On/Standby/Off")
+defineTumb("RADAR_LOW_ALT", devices.RADAR, device_commands.RADARlowAlt, 206, 0.5, {0, 1}, nil, false, "Right Vertical", "RADAR Low Altitude Off/Comp/On")
+defineToggleSwitch("RADAR_BEAM", devices.RADAR, device_commands.RADARfixBeam, 207, "Right Vertical", "RADAR Fixed Beam On/Off")
+definePushButton("RADAR_DISP_RST", devices.RADAR, device_commands.RADARmgnStir, 266, "Center Middle", "RADAR Screen Magnetic Reset")
+definePushButton("RADAR_CNT", devices.RADAR, device_commands.RADARjamCont, 330, "Center Middle", "RADAR Interferes - Continues")
+definePushButton("RADAR_TEMP", devices.RADAR, device_commands.RADARjamTmp, 331, "Center Middle", "RADAR Interferes - Temporary")
+definePushButton("RADAR_PASS", devices.RADAR, device_commands.RADARjamPass, 332, "Center Middle", "RADAR Interferes - Passive")
+definePushButton("RADAR_WX", devices.RADAR, device_commands.RADARjamMeteo, 333, "Center Middle", "RADAR Interferes - Weather")
+definePushButton("RADAR_IFF", devices.RADAR, device_commands.RADARvopros, 334, "Center Middle", "RADAR Interferes - IFF")
+definePushButton("RADAR_LOW", devices.RADAR, device_commands.RADARjamLowSpeed, 335, "Center Middle", "RADAR Interferes - Low Speed")
+definePushButton("RADAR_TEST", devices.RADAR, device_commands.RADARkontrol, 336, "Center Middle", "RADAR Interferes - Self-Test")
+definePushButton("RADAR_RST", devices.RADAR, device_commands.RADARreset, 337, "Center Middle", "RADAR Interferes - Reset")
+definePushButton("RADAR_LOCK", devices.RADAR, device_commands.Zahvat, 378, "Stick", "RADAR Lock Target")
+definePotentiometer("RADAR_FLT", devices.RADAR, device_commands.RADARFilter, 623, {0, 1}, "Center Middle", "RADAR Polar Filter")
+
+--SPRD/RATO
+defineToggleSwitch("SPRD_PWR", devices.SPRD, device_commands.SPRDstartOn, 167, "Right Horizontal", "SPRD On/Off")
+defineToggleSwitch("SPRD_DROP_PWR", devices.SPRD, device_commands.SPRDdropOn, 168, "Right Horizontal", "SPRD Drop System On/Off")
+defineToggleSwitch("SPRD_START_COVER", devices.SPRD, device_commands.SPRDstartSafetyCover, 252, "Center Middle", "SPRD Start Cover")
+definePushButton("SPRD_START", devices.SPRD, device_commands.SPRDstart, 253, "Center Middle", "SPRD Start")
+defineToggleSwitch("SPRD_DROP_COVER", devices.SPRD, device_commands.SPRDdropSafetyCover, 317, "Left Horizontal", "SPRD/Flare Drop Cover")
+definePushButton("SPRD_DROP", devices.SPRD, device_commands.SPRDdrop, 318, "Left Horizontal", "SPRD/Flare Drop")
+
+--SPS
+defineToggleSwitch("SPS_PWR", devices.SPS, device_commands.SPSon, 293, "Left Vertical", "SPS System On/Off")
+
+--ARU
+defineToggleSwitch("ARU_MODE", devices.ARU, device_commands.ARUmanAuto, 295, "Left Vertical", "ARU Auto/Manual")
+defineTumb("ARU_SPD", devices.ARU, device_commands.ARUhighSpeed, 296, 1, {-1, 1}, nil, false, "Left Vertical", "ARU Low/Neutral/High speed") --Momentary 3 position switch
+
+--Airbrake
+defineToggleSwitch("AIR_BRK", devices.AIRBRAKE, device_commands.Airbrakes, 316, "Left Horizontal", "Airbrakes")
+
+--Wheelbrakes
+defineToggleSwitch("ABS_ON", devices.GEAR_BRAKES, device_commands.ABSon, 299, "Left Vertical", "ABS On/Off")
+defineToggleSwitch("NW_BRK", devices.GEAR_BRAKES, device_commands.NosegearBrake, 238, "Center Upper", "Nosegear Brake On/Off")
+defineToggleSwitch("EM_BRK", devices.GEAR_BRAKES, device_commands.EmergBrakes, 237, "Center Middle", "Emergency Brake")
+
+--Gear
+defineToggleSwitch("GEAR_LOCK", devices.GEARS, device_commands.GearHandleFixator, 326, "Left Vertical", "Gear Handle Lock")
+defineTumb("GEAR_LVR", devices.GEARS, device_commands.GearLever, 327, 1, {-1, 1}, nil, false, "Left Vertical", "Gear Down/Neutral/Up")
+definePotentiometer("EM_GEAR_REL", devices.GEARS, device_commands.EmergGearsMain, 223, {0, 1}, "Right Vertical", "Emergency Gear Release")
+defineToggleSwitch("EM_NOSE_GEAR_REL", devices.GEARS, device_commands.EmergGearsNose, 281, "Center Lower", "Emergency Nose Gear Release")
+
+--Flaps
+defineToggleSwitch("FLAPS_NEUT", devices.FLAPS, device_commands.Flaps0, 311, "Left Horizontal", "Flaps Neutral")
+defineToggleSwitch("FLAPS_TO", devices.FLAPS, device_commands.Flaps25, 312, "Left Horizontal", "Flaps Take-off")
+defineToggleSwitch("FLAPS_LDG", devices.FLAPS, device_commands.Flaps45, 313, "Left Horizontal", "Flaps Landing")
+definePushButton("FLAPS_RST", devices.FLAPS, device_commands.FlapsReset, 314, "Left Horizontal", "Flaps Reset")
+
+--Drogue Chute
+definePushButton("CHUTE_REL", devices.CHUTE, device_commands.DragChute, 298, "Left Vertical", "Drogue Chute Release")
+defineToggleSwitch("CHUTE_DROP_CVR", devices.CHUTE, device_commands.DragChuteSafetyCover, 304, "Left Horizontal", "Drogue Chute Drop Cover")
+definePushButton("CHUTE_DROP", devices.CHUTE, device_commands.DragChuteDisconnect, 305, "Left Horizontal", "Drogue Chute Drop")
+
+--Trimmer
+defineToggleSwitch("TRM_PWR", devices.TRIMER, device_commands.TrimmerOn, 172, "Right Vertical", "Trimmer On/Off")
+defineTumb("TRM_ADJ", devices.TRIMER, device_commands.TrimmerBtnUp, 379, 1, {-1, 1}, nil, false, "Stick", "Trimmer Adjust")
+
+--Nosecone
+defineToggleSwitch("CONE_PWR", devices.KONUS, device_commands.KonusOn, 170, "Right Vertical", "Nosecone On/Off")
+defineToggleSwitch("CONE_MODE", devices.KONUS, device_commands.KonusManAuto, 309, "Left Vertical", "Nosecone Auto/Manual control")
+definePotentiometer("CONE_ADJ", devices.KONUS, device_commands.KonusButton, 236, {0, 1}, "Center Upper", "Nosecone Manual Adjust")
+
+--SOPLO
+defineToggleSwitch("NOZ_POS", devices.SOPLO, device_commands.Soplo2xPoz, 291, "Left Vertical", "Emergency Nozzle Position Control")
+
+--Main and buster hydro
+defineToggleSwitch("EMR_HYD_PUMP", devices.MAIN_HYDRO, device_commands.NR27on, 171, "Right Vertical", "Emergency Main Hydraulic Pump On/Off")
+defineToggleSwitch("AIL_BST", devices.MAIN_HYDRO, device_commands.AileronBoosters, 319, "Left Horizontal", "Aileron Boosters On/Off")
+
+--KPP
+defineToggleSwitch("KPP_MAIN_EMR", devices.KPP, device_commands.KPPon, 177, "Right Vertical", "KPP Main/Emergency")
+definePushButton("KPP_CGE", devices.KPP, device_commands.KPParretir, 259, "Center Middle", "KPP Cage")
+definePotentiometer("KPP_SET", devices.KPP, device_commands.KPPsetPitch, 260, {-1, 1}, "Center Middle", "KPP Set Pitch")
+
+--IAS/TAS/KSI (NPP)
+defineToggleSwitch("NPP_PWR", devices.KSI, device_commands.NPPon, 178, "Right Vertical", "NPP On/Off")
+definePushButton("NPP_ADJ", devices.KSI, device_commands.NPPnastrojka, 258, "Center Middle", "NPP Adjust")
+definePotentiometer("NPP_CRS", devices.KSI, device_commands.NPPsetCourse, 263, {0, 1}, "Center Middle", "NPP Course Set")
+
+--ALT and RALT
+defineToggleSwitch("RALT_PWR", devices.RADIO_ALTIMETER, device_commands.RadioAltOn, 175, "Right Vertical", "Radio Altimeter On")
+defineTumb("ALT_WARN", devices.RADIO_ALTIMETER, device_commands.DangerAltSelect, 284, 0.14, {0.0, 0.98}, nil, false, "Center Lower", "Altitude Warning Alarm Adjust")
+definePotentiometer("ALT_PRESS", devices.ALTIMETER, device_commands.AltimeterPressure, 262, {-1, 1}, "Center Middle", "Altimeter Pressure Knob")
+
+--Oxygen System
+defineToggleSwitch("HELM_AIR", devices.OXYGENE_SYSTEM, device_commands.HelmetAirCondition, 285, "Left Horizontal", "Helmet Air Conditioning On/Off")
+defineToggleSwitch("EMR_O2", devices.OXYGENE_SYSTEM, device_commands.EmergOxygene, 286, "Left Horizontal", "Emergency Oxygen")
+defineToggleSwitch("MIX_O2", devices.OXYGENE_SYSTEM, device_commands.MixtureOxygeneSelect, 287, "Left Horizontal", "Mixture/Oxygen")
+
+--Canopy
+defineToggleSwitch("CAB_PRES", devices.CANOPY, device_commands.CanopyHermetHandle, 328, "Left Vertical", "Pressurize Canopy")
+defineToggleSwitch("CAN_LCK", devices.CANOPY, device_commands.CanopyLockHandle, 329, "Left Vertical", "Lock Canopy")
+definePushButton("CAN_OPN", devices.CANOPY, device_commands.CanopyOpen, 375, "Left Vertical", "Open Canopy")
+definePushButton("CAN_CLS", devices.CANOPY, device_commands.CanopyClose, 385, "Center Upper", "Close Canopy")
+definePushButton("CAN_ANTI_ICE", devices.CANOPY, device_commands.CanopyAntiIce, 239, "Center Upper", "Canopy Anti Ice")
+defineToggleSwitch("CAN_EMR_REL", devices.CANOPY, device_commands.CanopyEmergRelease, 224, "Right Vertical", "Canopy Emergency Jettison")
+definePotentiometer("CAN_VENT", devices.CANOPY, device_commands.CanopyVentSystem, 649, {0, 1}, "Right Vertical", "Canopy Ventilation")
+
+--ASP Gunsight
+defineToggleSwitch("SIGHT_PWR", devices.ASP, device_commands.ASPon, 186, "Right Vertical", "Gunsight On/Off")
+defineToggleSwitch("SIGHT_MAN_AUTO", devices.ASP, device_commands.ASPmanAutoSelect, 241, "Gunsight", "Gunsight Auto/Manual")
+defineToggleSwitch("SIGHT_SHT_BMB", devices.ASP, device_commands.ASPbombStrelbSelect, 242, "Gunsight", "Gunsight Shoot/Bomb Select")
+defineToggleSwitch("SIGHT_MSL_GUN", devices.ASP, device_commands.ASPmissileGunSelect, 243, "Gunsight", "Gunsight gun/missile Select")
+defineToggleSwitch("SIGHT_GYRO_MSL", devices.ASP, device_commands.ASPssGiroSelect, 244, "Gunsight", "Gunsight Gyro/Missile Select")
+defineToggleSwitch("SIGHT_PPR", devices.ASP, device_commands.ASPpipperOn, 249, "Gunsight", "Gunsight Pipper On/Off")
+defineToggleSwitch("SIGHT_FIX_NET", devices.ASP, device_commands.ASPnetOn, 250, "Gunsight", "Gunsight Fixed Net On/Off")
+definePotentiometer("TGT_SIZE", devices.ASP, device_commands.ASPtargetSize, 245, {0, 1}, "Gunsight", "Gunsight Target Range")
+definePotentiometer("INT_ANGLE", devices.ASP, device_commands.ASPinterceptAngle, 246, {0, 1}, "Gunsight", "Gunsight Intercept Angle")
+definePotentiometer("SCL_BRT", devices.ASP, device_commands.ASPscaleLight, 247, {0, 1}, "Gunsight", "Gunsight Scale Brightness")
+definePotentiometer("PPR_BRT", devices.ASP, device_commands.ASPpipperLight, 248, {0, 1}, "Gunsight", "Gunsight Pipper Brightness")
+definePotentiometer("FIX_NET_BRT", devices.ASP, device_commands.ASPnetLight, 251, {0, 1}, "Gunsight", "Gunsight Fixed Net Brightness")
+definePotentiometer("TDC_RGE_SPAN", devices.ASP, device_commands.RUDoblog_axis, 384, {0, 1}, "Left Horizontal", "TDC Range/Pipper Span")
+
+--Weapon Control
+defineToggleSwitch("MSL_HEAT", devices.WEAPON_CONTROL, device_commands.Obogrev, 181, "Right Vertical", "Missile/Rocket Heat")
+defineToggleSwitch("MSL_LNCH", devices.WEAPON_CONTROL, device_commands.Pusk, 182, "Right Vertical", "Missile/Rocket Launch")
+defineToggleSwitch("INB_PYL_PWR", devices.WEAPON_CONTROL, device_commands.Pitanie12, 183, "Right Vertical", "Pylons 1-2 On/Off")
+defineToggleSwitch("OBD_PYL_PWR", devices.WEAPON_CONTROL, device_commands.Pitanie34, 184, "Right Vertical", "Pylons 3-4 On/Off")
+defineToggleSwitch("GUN_PWR", devices.WEAPON_CONTROL, device_commands.GS23, 185, "Right Vertical", "GS-23 Gun On/Off")
+defineToggleSwitch("GUN_CAM", devices.WEAPON_CONTROL, device_commands.FKP, 187, "Right Vertical", "Gun Camera On/Off")
+defineToggleSwitch("BMB_FUZ_CVR", devices.WEAPON_CONTROL, device_commands.TaktSbrosSafetyCover, 277, "Center Lower", "Tactical Drop Cover")
+defineToggleSwitch("BMB_FUZ", devices.WEAPON_CONTROL, device_commands.TaktSbros, 278, "Center Lower", "Tactical Drop")
+defineToggleSwitch("EM_MSL_JET_CVR", devices.WEAPON_CONTROL, device_commands.AvarPuskSafetyCover, 275, "Center Lower", "Emergency Missile/Rocket Launch Cover")
+definePushButton("EM_MSL_JET", devices.WEAPON_CONTROL, device_commands.AvarPusk, 276, "Center Lower", "Emergency Missile/Rocket Launch")
+defineToggleSwitch("WING_DRP_TKS_CVR", devices.WEAPON_CONTROL, device_commands.SbrosKrilBakovSafetyCover, 256, "Center Middle", "Wing Drop Tank Jettison Cover")
+definePushButton("WING_DRP_TKS", devices.WEAPON_CONTROL, device_commands.SbrosKrilBakov, 257, "Center Middle", "Wing Drop Tank Jettison")
+definePushButton("CTR_DRP_TK", devices.WEAPON_CONTROL, device_commands.SbrosPodvBakov, 386, "Stick", "Centerline Drop Tank Jettison")
+defineToggleSwitch("OBD_PYL_JET_CVR", devices.WEAPON_CONTROL, device_commands.SbrosVnesnSafetyCover, 269, "Center Middle", "Outboard Pylon Jettison Cover")
+definePushButton("OBD_PYL_JET", devices.WEAPON_CONTROL, device_commands.SbrosVnesn, 270, "Center Middle", "Outboard Pylon Jettison")
+defineToggleSwitch("INB_PYL_JET_CVR", devices.WEAPON_CONTROL, device_commands.SbrosVnutrSafetyCover, 271, "Center Middle", "Inboard Pylon Jettison Cover")
+definePushButton("INB_PYL_JET", devices.WEAPON_CONTROL, device_commands.SbrosVnutr, 272, "Center Middle", "Inboard Pylon Jettison")
+defineToggleSwitch("MODE_AA_AG", devices.WEAPON_CONTROL, device_commands.ASPvozduhZemlja, 230, "Center Upper", "Weapons AA/AG Mode")
+defineTumb("IR_SAR_NEU", devices.WEAPON_CONTROL, device_commands.ASPssNeutrRns, 231, 0.5, {0, 1}, nil, false, "Center Upper", "IR/Neutral/SAR Missile Mode")
+definePushButton("GUN_PYRO_1", devices.WEAPON_CONTROL, device_commands.ASPgunReload1, 232, "Center Upper", "Gun Pyro Charge 1")
+definePushButton("GUN_PYRO_2", devices.WEAPON_CONTROL, device_commands.ASPgunReload2, 233, "Center Upper", "Gun Pyro Charge 2")
+definePushButton("GUN_PYRO_3", devices.WEAPON_CONTROL, device_commands.ASPgunReload3, 234, "Center Upper", "Gun Pyro Charge 3")
+defineTumb("WPN_SEL", devices.WEAPON_CONTROL, device_commands.ASPlauncherSelect, 235, .1, {0, 1}, nil, true, "Center Upper", "Weapon Selector")
+definePotentiometer("MSL_VOL", devices.WEAPON_CONTROL, device_commands.MissileSound, 297, {0, 1}, "Left Vertical", "IR Missile Seeker Volume")
+definePushButton("GUN", devices.WEAPON_CONTROL, device_commands.GunFireBtn, 381, "Stick", "Gun Trigger")
+definePushButton("WPN_REL", devices.WEAPON_CONTROL, device_commands.PuskBtn, 382, "Stick", "Weapons Release")
+defineToggleSwitch("WPN_REL_CVR", devices.WEAPON_CONTROL, device_commands.PuskBtnSafetyCover, 383, "Stick", "Weapons Release Cover")
+
+--Helmet Visor
+defineToggleSwitch("HELM_HEAT_MODE", devices.HELMET_VISOR, device_commands.HelmetHeatManAut, 306, "Left Vertical", "Helmet Heat Auto/Manual")
+definePushButton("HELM_QUICK_HEAT", devices.HELMET_VISOR, device_commands.HelmetQuickHeat, 310, "Left Vertical", "Helmet Quick Heat")
+defineToggleSwitch("HELM_VSR", devices.HELMET_VISOR, device_commands.HelmetVisor, 369, "Left Vertical", "Helmet Visor On/Off")
+
+--Air Conditioning
+defineTumb("CPT_AIR_CND", devices.AIR_CONDITIONING, device_commands.AirConditioningSelect, 292, 0.33, {0, 0.99}, nil, true, "Left Vertical", "Air Conditioning Select")
+
 devices = nil
 device_commands = nil
 
