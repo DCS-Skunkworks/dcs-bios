8faad87a88edb3f84f1a535366fb88e41366eace
Jan Böcker <jan.boecker@jboecker.de>
2016-03-03 23:15:18 +0100

    change baud rate to 250000 to adapt to changes in v0.2.0 of the Arduino library

--- connect-serial-port.cmd
+++ connect-serial-port.cmd
@@ -5,7 +5,7 @@ set COMPORT=5
 
 
 set /A TTYNUM=%COMPORT%-1
-mode COM%COMPORT% BAUD=500000 PARITY=N DATA=8 STOP=1 TO=off DTR=on
+mode COM%COMPORT% BAUD=250000 PARITY=N DATA=8 STOP=1 TO=off DTR=on
 socat\socat -v UDP4-RECV:5010,ip-add-membership=239.255.50.10:127.0.0.1,reuseaddr!!udp-sendto:localhost:7778 /dev/ttyS%TTYNUM%
 
 pause
