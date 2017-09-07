a0752025642bb8bf1137a6c64d78a8603e4c4e38
kadda11 <kadda11@hotmail.com>
2017-09-07 21:55:29 +0200

    Add files via upload
    hi, this is my little contribution to this exellent project, 
    the L-39ZA.lua file contains the gauges and lamps for the l-39za,
    the second file contains alsaw the switches and "jazzymanserg" helped me to add them,
    
    for the moment i didn t add the L-39C...

---
+++ AircraftList.lua
@@ -0,0 +1,21 @@
+BIOS.ALL_PLAYABLE_AIRCRAFT = {}
+BIOS.CLICKABLE_COCKPIT_AIRCRAFT = {}
+BIOS.FLAMING_CLIFFS_AIRCRAFT = {}
+local function a(name, hasClickableCockpit)
+	BIOS.ALL_PLAYABLE_AIRCRAFT[#BIOS.ALL_PLAYABLE_AIRCRAFT+1] = name
+	if hasClickableCockpit then
+		BIOS.CLICKABLE_COCKPIT_AIRCRAFT[#BIOS.CLICKABLE_COCKPIT_AIRCRAFT+1] = name
+	else
+		BIOS.FLAMING_CLIFFS_AIRCRAFT[#BIOS.FLAMING_CLIFFS_AIRCRAFT+1] = name
+	end
+end
+
+a("A-10C", true)
+a("Ka-50", true)
+a("L-39ZA", true)
+a("Mi-8MT", true)
+a("P-51D", true)
+a("Su-25T", false)
+a("TF-51D", true)
+a("UH-1H", true)
+a("MiG-21Bis", true)
