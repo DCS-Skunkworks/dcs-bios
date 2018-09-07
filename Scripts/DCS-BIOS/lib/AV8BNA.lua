5ee1a310d2643e4848425375121417b0c40e0610
WarLord <warlord12@freenet.de>
2018-09-07 16:47:06 +0200

    AV8BNA Updates

--- Scripts/DCS-BIOS/lib/AV8BNA.lua
+++ Scripts/DCS-BIOS/lib/AV8BNA.lua
@@ -31,10 +31,27 @@ local function define3PosTumb(msg, device_id, command, arg_number, category, des
 end
 
 
+-------------------------------------------------------------------
+-- MAIN INSTRUMENT PANEL:
+-------------------------------------------------------------------
+
 -- Master Modes Panel (MSC)
 definePushButton("NAV_SEL", 12, 3282, 282,"Master Modes" , "NAV Master Mode Selector")
 definePushButton("VSTOL_SEL", 12, 3284, 284,"Master Modes" , "VSTOL Master Mode Selector")
 definePushButton("AG_SEL", 12, 3280, 280,"Master Modes" , "AG Master Mode Selector")
+
+-- Engine Display Panel
+definePotentiometer("EDP_BRIGHT", 20, 3272, 272, {-1, 1},"Engine Display" , "EDP Brightness Control")
+defineToggleSwitch("EDP_BIT", 20, 3655, 655,"Engine Display" , "EDP BIT Button")
+
+-- HUD Control Panel
+
+
+
+
+-- Landing Gear/Flaps Control Panel
+defineToggleSwitch("LG_LEVER", 28, 3461, 461,"Gear/Flaps Control Panel" , "Gear Lever")
+defineToggleSwitch("LG_EMERGBATT_LEVER", 28, 3470, 470,"Gear/Flaps Control Panel" , "Landing Gear Emergency Battery Lever")
 
 
 BIOS.protocol.endModule()
