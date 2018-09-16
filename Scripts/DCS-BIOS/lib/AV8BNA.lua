e5310b54c1be4e3ede3ffb1a96684963e35c572a
WarLord <warlord12@freenet.de>
2018-09-16 16:24:48 +0200

    AV8BNA Updates

--- Scripts/DCS-BIOS/lib/AV8BNA.lua
+++ Scripts/DCS-BIOS/lib/AV8BNA.lua
@@ -41,10 +41,16 @@ definePushButton("VSTOL_SEL", 12, 3284, 284,"Master Modes" , "VSTOL Master Mode 
 definePushButton("AG_SEL", 12, 3280, 280,"Master Modes" , "AG Master Mode Selector")
 
 -- Engine Display Panel
-definePotentiometer("EDP_BRIGHT", 20, 3272, 272, {-1, 1},"Engine Display" , "EDP Brightness Control")
+definePotentiometer("EDP_BRIGHT", 20, 3272, 272, {0, 1},"Engine Display" , "EDP Brightness Control")
 defineToggleSwitch("EDP_BIT", 20, 3655, 655,"Engine Display" , "EDP BIT Button")
 
 -- HUD Control Panel
+define3PosTumb("HUD_SYMBOL", 22, 3288, 288,"HUD Control" , "HUD Symbology Reject Switch NORM/REJ1/REJ2")
+definePotentiometer("HUD_BRIGHT", 22, 3289, 289, {0, 1},"HUD Control" , "HUD Off/Brightness Control")
+define3PosTumb("HUD_MODE", 22, 3290, 290,"HUD Control" , "HUD Display Mode Switch DAY/AUTO/NIGHT")
+definePotentiometer("HUD_VIDEO_BRIGHT", 22, 3291, 291, {0, 1},"HUD Control" , "HUD Video Brightness Control")
+definePotentiometer("HUD_VIDEO_CONTR", 22, 3292, 292, {0, 1},"HUD Control" , "HUD Video Contrast Control")
+defineToggleSwitch("HUD_ALT", 22, 3293, 293,"HUD Control" , "HUD Altitude Selector Switch")
 
 -- UFC Panel
 definePushButton("UFC_B1", 23, 3302, 302,"UFC" , "UFC Button 1")
@@ -94,6 +100,9 @@ definePushButton("M_Caution", 35, 3198, 198,"Master Warning Panel" , "Master Cau
 definePushButton("M_Warning", 35, 3199, 199,"Master Warning Panel" , "Master Warning")
 
 -- Fuel Quantity Indicator
+defineMultipositionSwitch("FUEL_SEL", 21, 3379, 379, 7, 0.33,"Fuel Panel" ,"Fuel Totalizer Selector OUTBD/INBD/WING/INT/TOT/FEED/BIT")
+definePotentiometer("BINGO_SET", 21, 3380, 380, {0, 1},"Fuel Panel" , "Bingo Fuel Set Knob")
+
 -- MPCD left
 definePushButton("MPCD_L_1", 26, 3200, 200,"MPCD Left" , "MPCD Left Button 1")
 definePushButton("MPCD_L_2", 26, 3201, 201,"MPCD Left" , "MPCD Left Button 2")
@@ -115,6 +124,8 @@ definePushButton("MPCD_L_17", 26, 3216, 216,"MPCD Left" , "MPCD Left Button 17")
 definePushButton("MPCD_L_18", 26, 3217, 217,"MPCD Left" , "MPCD Left Button 18")
 definePushButton("MPCD_L_19", 26, 3218, 218,"MPCD Left" , "MPCD Left Button 19")
 definePushButton("MPCD_L_20", 26, 3219, 219,"MPCD Left" , "MPCD Left Button 20")
+
+definePotentiometer("MPCD_L_BRIGHT", 26, 3194, 194, {0, 1},"MPCD Left" , "MPCD Left Off/Brightness Control")
 
 -- MPCD Right
 definePushButton("MPCD_R_1", 27, 3224, 224,"MPCD Right" , "MPCD Right Button 1")
@@ -138,6 +149,8 @@ definePushButton("MPCD_R_18", 27, 3241, 241,"MPCD Right" , "MPCD Right Button 18
 definePushButton("MPCD_R_19", 27, 3242, 242,"MPCD Right" , "MPCD Right Button 19")
 definePushButton("MPCD_R_20", 27, 3243, 243,"MPCD Right" , "MPCD Right Button 20")
 
+definePotentiometer("MPCD_R_BRIGHT", 27, 3195, 195, {0, 1},"MPCD Right" , "MPCD Right Off/Brightness Control")
+
 -- Armament Control Panel ACP
 
 -- Master Armament Panel
@@ -151,6 +164,9 @@ defineToggleSwitch("LG_LEVER", 28, 3461, 461,"Gear/Flaps Control Panel" , "Gear 
 defineToggleSwitch("LG_EMERGBATT_LEVER", 28, 3470, 470,"Gear/Flaps Control Panel" , "Landing Gear Emergency Battery Lever")
 
 -- Combat/Water Panel
+definePushButton("CMBT", 9, 3450, 450,"H2O" , "CMBT Thrust Button")
+define3PosTumb("H2O_MODE", 9, 3449, 449,"H2O" , "H2O Mode Switch  TO/OFF/LDG")
+
 -- Bulkhead Switches
 -- Seat
 defineToggleSwitch("SEAT_SAFE_LEVER", 12, 3800, 800,"Seat" , "Seat Ground Safety Lever")
@@ -172,18 +188,61 @@ defineMultipositionSwitch("INS_MODE", 11, 3421, 421, 7, 0.1,"INS" ,"INS Mode Kno
 -- Throttle Quadrant
 -- Fuel Panel
 -- External Lights Panel
+
 -- Pilot Service Panel
+defineToggleSwitch("O2_SW", 36, 3514, 514,"Gear/Flaps Control Panel" , "Oxygen Switch")
+defineToggleSwitch("H2O_DUMP", 9, 3515, 515,"Gear/Flaps Control Panel" , "H2O Dump Switch")
+defineToggleSwitch("LIDS_SW", 13, 3516, 516"Gear/Flaps Control Panel" , "LIDS Switch")
+defineToggleSwitch("RPM_SW", 9, 3517, 517,"Gear/Flaps Control Panel" , "ENG RPM Switch")
+defineToggleSwitch("EFC_SW", 9, 3518, 518,"Gear/Flaps Control Panel" , "EFC Switch")
+
 -- Bulkhead Switches
 
 -------------------------------------------------------------------
 -- RIGHT CONSOLE:
 -------------------------------------------------------------------
 -- Electrical Panel
+define3PosTumb("DC_TEST_SW", 1, 3609, 609,"Electric Panel" , "DC Test Switch")
+define3PosTumb("APU_GEN_SW", 1, 3610, 610,"Electric Panel" , "APU Generator Switch RESET/ON/OFF")
+defineToggleSwitch("ENG_START_SW", 1, 3611, 611,"Electric Panel" , "Engine Start Switch")
+define3PosTumb("GEN_SW", 1, 3612, 612,"Electric Panel" , "Generator Switch  ON/OFF/TEST")
+define3PosTumb("BATT_SW", 1, 3613, 613,"Electric Panel" , "Battery Switch  ON/OFF/ALERT")
+
 -- V/UHF Radio Panel
+definePotentiometer("UHF_VOL", 7, 3614, 614, {0, 1}, "UHF Radio", "V/UHF RSC Volume Knob")
+definePotentiometer("UHF_FREQ", 7, 3615, 615, {0, 1}, "UHF Radio", "V/UHF RSC Chan/Freq Knob")
+defineMultipositionSwitch("UHF_MODE", 7, 3616, 616, 7, 0.2,"UHF Radio" ,"V/UHF RSC Operational Mode Switch ZRO/OFF/TEST/TR+G/TR/ADF/CHNG PRST")
+definePushButton("UHF_ANC_POINT", 7, 3617, 617,"UHF Radio" , "V/UHF RSC Ancillary Mode Pointer")
+definePushButton("UHF_ANC_SW", 7, 3618, 618,"UHF Radio" , "V/UHF RSC Ancillary Mode Switch")
+defineMultipositionSwitch("BOMBS_ARM_SW", 7, 3619, 619, 8, 0.15,"UHF Radio" ,"V/UHF RSC Frequency Mode Switch AJ-M/AJ/MAR/PRST/MAN/243/121")
+definePushButton("UHF_LOAD_SW", 7, 3620, 620,"UHF Radio" , "V/UHF RSC LOAD/OFST Switch")
+
 -- ACNIP
+defineToggleSwitch("ACNIP_MODE", 8, 3621, 621,"ACNIP" , "ACNIP Mode Switch")
+defineToggleSwitch("KY1_TYPE", 8, 3622, 622,"ACNIP" , "KY-1 Cipher Type Selector Switch")
+defineToggleSwitch("KY2_TYPE", 8, 3623, 623,"ACNIP" , "KY-2 Cipher Type Selector Switch")
+defineToggleSwitch("KY1_MODE", 8, 3624, 624,"ACNIP" , "KY-1 Code/Mode Switch")
+defineToggleSwitch("KY2_MODE", 8, 3625, 625,"ACNIP" , "KY-2 Code/Mode Switch")
+defineToggleSwitch("ACNIP_RADIO_SEL", 8, 3626, 626,"ACNIP" , "ACNIP Radio Selector Switch")
+defineToggleSwitch("KY58_CODE_CLEAR", 8, 3627, 627,"ACNIP" , "KY-58 Codes Clear Switch")
+define3PosTumb("KY58_CODE_LOAD", 8, 3628, 628,"ACNIP" , "KY-58 Remote Codes Load Switch  RV1/OPR/RV2")
+
 -- ICS
+definePotentiometer("ICS_AUX_VOL", 4, 3629, 629, {0, 1}, "ICS", "ICS Aux Volume Knob")
+definePotentiometer("ICS_GND_VOL", 4, 3630, 630, {0, 1}, "ICS", "ICS Ground Volume Knob")
+define3PosTumb("ICS_MIC_MODE", 4, 3631, 631,"ICS" , "ICS Mic Operational Mode Switch  TEL/HOT/COLD")
+
 -- IFF
+defineToggleSwitch("IFF_OPER_MODE", 8, 3632, 632,"IFF" , "IFF Operational Mode Switch")
+define3PosTumb("IFF_CRYPT_MODE", 8, 3633, 633,"IFF" , "IFF Crypto Mode Switch  ZERO/NORM/HOLD")
+
 -- Interior Lights Panel
+define3PosTumb("COMP_LIGHT", 34, 3634, 634,"Int Light" , "Compass Light/Test Lights  COPM/OFF/LTS TEST")
+definePotentiometer("INST_LIGHTS", 34, 3635, 635, {0, 1}, "Int Light", "Instruments Lights")
+definePotentiometer("CONSOLE_LIGHTS", 34, 3636, 636, {0, 1}, "Int Light", "Console Lights")
+definePotentiometer("FLOOD_LIGHTS", 34, 3637, 637, {0, 1}, "Int Light", "Flood Lights")
+definePotentiometer("ANNUNCIATOR_LIGHTS", 34, 3638, 638, {0, 0.9}, "Int Light", "Annunciator Lights")
+
 -- ECS Panel
 
 -------------------------------------------------------------------
