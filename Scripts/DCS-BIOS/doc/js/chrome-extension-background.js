aaaecf47fe227ba6e1f1e0f772ae7a28d8d7a9bf
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2019-01-13 15:16:43 +0200

    Control Ref Page size increased.

--- Scripts/DCS-BIOS/doc/js/chrome-extension-background.js
+++ Scripts/DCS-BIOS/doc/js/chrome-extension-background.js
@@ -1,3 +1,18 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create("control-reference.html");
-});
+chrome.app.runtime.onLaunched.addListener(
+	function() {
+		chrome.app.window.create(
+			'control-reference.html',
+			{
+				innerBounds: {
+					width: 500, minWidth: 500, maxWidth: 500,
+					height: 900, minHeight: 900, maxHeight: 900
+				},
+				'resizable' : true
+			},
+			function(createdWindow) {
+				createdWindow.innerBounds.height = 800;
+				createdWindow.innerBounds.width = 500;
+			}
+		);
+	}
+);
