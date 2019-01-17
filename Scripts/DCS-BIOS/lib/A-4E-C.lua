ac2bf27822eabcfd9e664e8b9dcf48b4a827ed1e
WarLord <warlord12@freenet.de>
2019-01-17 17:59:43 +0100

    fixes

--- Scripts/DCS-BIOS/lib/A-4E-C.lua
+++ Scripts/DCS-BIOS/lib/A-4E-C.lua
@@ -37,6 +37,8 @@ defineIndicatorLight("OIL_LIGHT", 150, "Warning, Caution and IndicatorLights","O
 defineIndicatorLight("AOA_GREEN", 850, "Warning, Caution and IndicatorLights","AoA Green")
 defineIndicatorLight("AOA_YELLOW", 851, "Warning, Caution and IndicatorLights","AoA Yellow")
 defineIndicatorLight("AOA_RED", 852, "Warning, Caution and IndicatorLights","AoA Red")
+defineIndicatorLight("APC_LIGHT", 147, "Warning, Caution and IndicatorLights","APC Light")
+
 
 -- Gauges
 defineFloat("ENGINE_RPM", 520, {0.0, 1.0}, "Gauges", "Engine RPM")
@@ -44,15 +46,25 @@ defineFloat("AOA", 840, {0.0, 1.0}, "Gauges", "Angle of Attack")
 ----------------------------------------------------------------------------------------------------------------
 -- Landing Gear & Tail Hook
 defineToggleSwitch("GEAR_HANDLE", 12, 3020, 8,"Gear" , "Landing Gear Handle")
-defineToggleSwitch("HOOK_HANDLE", 12, 10032, 10,"Gear" , "Landing Hook Handle")
+--defineToggleSwitch("HOOK_HANDLE", 12, 10032, 10,"Gear" , "Landing Hook Handle")
+
+--Speedbrake
+defineToggleSwitch("SPEED_B_SW", 10, 3024, 85, "Speedbrake" , "Speedbrake switch")
+define3PosTumb("SPEED_B_EMERG", 10, 3035, 128, "Speedbrake", "Speedbrake emergency")
 
 --Spoilers
-defineToggleSwitch("SPOILER_ARM", 13, 10003, 84,"Spoiler" , "Spoiler Arm Switch")
+defineToggleSwitch("SPOILER_ARM", 13, 3017, 84,"Spoiler" , "Spoiler Arm Switch")
+
 -- APPROACH POWER COMPENSATOR PANEL #17A
 define3PosTumb("APC_ON", 26, 10121, 135, "Electric", "APC Enable/Stby/Off")
+
 -- ALTIMETER PANEL #41
-definePotentiometer("SET_ALT", 21, 10028, 827, {0, 1}, "Main Panel", "Altimeter Setting")
+definePotentiometer("SET_ALT", 21, 3019, 827, {0, 1}, "Main Panel", "Altimeter Setting")
+
 -- RADAR CONTROL PANEL #6
-defineMultipositionSwitch("RADAR_MODE", 7, 10073, 120, 5, 0.1,"Radar" ,"Radar Mode Switch")
+defineTumb("RADAR_MODE", 7, 3063, 120, 0.1, {0.0, 0.4}, nil, false, "Radar" ,"Radar Mode Switch")
+
+--EXTERNAL LIGHTS #24
+define3PosTumb("MASTER_LIGHT", 24, 3073, 83, "External Lights", "Master Lighting ON/OFF/Momentary")
 
 BIOS.protocol.endModule()
