c7dd2b033c8580521a2ba656554a73c23711616c
WarLord <warlord12@freenet.de>
2019-01-31 18:06:18 +0100

    cleanup AV8BNA

--- Scripts/DCS-BIOS/lib/AV8BNA.lua
+++ Scripts/DCS-BIOS/lib/AV8BNA.lua
@@ -9,22 +9,15 @@ local document = BIOS.util.document
 local parse_indication = BIOS.util.parse_indication
 
 
-local defineIndicatorLight = BIOS.util.defineIndicatorLight
+--local defineIndicatorLight = BIOS.util.defineIndicatorLight
 local definePushButton = BIOS.util.definePushButton
 local definePotentiometer = BIOS.util.definePotentiometer
-local defineRotary = BIOS.util.defineRotary
-local defineSetCommandTumb = BIOS.util.defineSetCommandTumb
 local defineTumb = BIOS.util.defineTumb
 local defineToggleSwitch = BIOS.util.defineToggleSwitch
-local defineToggleSwitchToggleOnly = BIOS.util.defineToggleSwitchToggleOnly
 local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
-local defineVariableStepTumb = BIOS.util.defineVariableStepTumb
-local defineString = BIOS.util.defineString
-local defineRockerSwitch = BIOS.util.defineRockerSwitch
+--local defineString = BIOS.util.defineString
 local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
-local defineElectricallyHeldSwitch = BIOS.util.defineElectricallyHeldSwitch
-local defineFloat = BIOS.util.defineFloat
-local define8BitFloat = BIOS.util.define8BitFloat
+--local defineFloat = BIOS.util.defineFloat
 
 local function define3PosTumb(msg, device_id, command, arg_number, category, description)
 	defineTumb(msg, device_id, command, arg_number, 1, {-1, 1}, nil, false, category, description)
