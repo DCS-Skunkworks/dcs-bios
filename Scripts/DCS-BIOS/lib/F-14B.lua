f49a50044b16a742a1816bf76e6d783185e6eba9
WarLord <warlord12@freenet.de>
2019-03-14 17:39:30 +0100

    Add F-14

---
+++ Scripts/DCS-BIOS/lib/F-14B.lua
@@ -0,0 +1,29 @@
+BIOS.protocol.beginModule("F-14B", 0x1200)
+BIOS.protocol.setExportModuleAircrafts({"F-14B"})
+
+local documentation = moduleBeingDefined.documentation
+
+local document = BIOS.util.document
+
+local parse_indication = BIOS.util.parse_indication
+
+local defineIndicatorLight = BIOS.util.defineIndicatorLight
+local definePushButton = BIOS.util.definePushButton
+local definePotentiometer = BIOS.util.definePotentiometer
+local defineRotary = BIOS.util.defineRotary
+local defineTumb = BIOS.util.defineTumb
+local defineToggleSwitch = BIOS.util.defineToggleSwitch
+local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
+local defineVariableStepTumb = BIOS.util.defineVariableStepTumb
+local defineString = BIOS.util.defineString
+local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
+local defineFloat = BIOS.util.defineFloat
+local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
+
+local function define3PosTumb(msg, device_id, command, arg_number, category, description)
+	defineTumb(msg, device_id, command, arg_number, 1, {-1, 1}, nil, false, category, description)
+end
+
+
+
+BIOS.protocol.endModule()
