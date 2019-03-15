8bb9f570c830e0746b620e80da9e7f377b18659d
WarLord <warlord12@freenet.de>
2019-03-15 15:11:48 +0100

    F-14 more switches, Mig-19 More Gauges

--- Scripts/DCS-BIOS/lib/F-14B.lua
+++ Scripts/DCS-BIOS/lib/F-14B.lua
@@ -9,23 +9,37 @@ local parse_indication = BIOS.util.parse_indication
 
 local defineIndicatorLight = BIOS.util.defineIndicatorLight
 local definePushButton = BIOS.util.definePushButton
-local definePotentiometer = BIOS.util.definePotentiometer
-local defineRotary = BIOS.util.defineRotary
+--local definePotentiometer = BIOS.util.definePotentiometer
+--local defineRotary = BIOS.util.defineRotary
 local defineTumb = BIOS.util.defineTumb
 local defineToggleSwitch = BIOS.util.defineToggleSwitch
-local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
-local defineVariableStepTumb = BIOS.util.defineVariableStepTumb
-local defineString = BIOS.util.defineString
-local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
+--local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
+--local defineVariableStepTumb = BIOS.util.defineVariableStepTumb
+--local defineString = BIOS.util.defineString
+--local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
 local defineFloat = BIOS.util.defineFloat
-local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
+--local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
 
 local function define3PosTumb(msg, device_id, command, arg_number, category, description)
 	defineTumb(msg, device_id, command, arg_number, 1, {-1, 1}, nil, false, category, description)
 end
 
 -- Hydraulics
-defineToggleSwitch("HYD_TRANS_PUMP_SW", 12, 3001, 629, "Hydraulics", "Hydraulic Transfer Pump Switch")
-defineToggleSwitch("HYD_TRANS_PUMP_COVER", 12, 3002, 630, "Hydraulics", "Hydraulic Transfer Pump Switch Cover")
+defineToggleSwitch("P_HYD_TRANS_PUMP_SW", 12, 3001, 629, "Hydraulics", "Pilot Hydraulic Transfer Pump Switch")
+defineToggleSwitch("P_HYD_TRANS_PUMP_COVER", 12, 3002, 630, "Hydraulics", "Pilot Hydraulic Transfer Pump Switch Cover")
+defineToggleSwitch("P_HYD_ISOL_SW", 12, 3005, 631, "Hydraulics", "Pilot Hydraulic Isolation Switch")
+defineToggleSwitch("P_HYD_EMERG_FCONTR_SW", 12, 3003, 928, "Hydraulics", "Pilot Hydraulic Emergency Flight Control Switch")
+defineToggleSwitch("P_HYD_EMERG_FCONTR_COVER", 12, 3004, 615, "Hydraulics", "Pilot Hydraulic Emergency Flight Control Switch Cover")
+defineToggleSwitch("P_HYD_HAND_PUMP", 12, 3006, 632, "Hydraulics", "Pilot Hydraulic Hand Pump")
+
+-- MASTER RESET
+definePushButton("P_MASTER RESET", 22, 3058, 1071, "MASTER RESET", "Pilot MASTER RESET")
+
+-- AICS
+defineToggleSwitch("P_INLET_RAMPS_L", 13, 3007, 2100, "AICS", "Pilot Stow Inlet Ramps Left Switch")
+defineToggleSwitch("P_INLET_RAMPS_R", 13, 3008, 2101, "AICS", "Pilot Stow Inlet Ramps Right Switch")
+
+-- Wing Sweep
+defineToggleSwitch("P_EMERG_WING_SWEEP_COVER", 16, 3029, 317, "Wing Sweep", "Pilot Emergency Wing Sweep Handle Cover")
 
 BIOS.protocol.endModule()
