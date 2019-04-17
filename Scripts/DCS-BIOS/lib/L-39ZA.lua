434bf7d1a8bcc59fa79af14093d84d007237c907
WarLord <warlord12@freenet.de>
2019-04-17 17:15:20 +0200

    L-39 corrections and additions

--- Scripts/DCS-BIOS/lib/L-39ZA.lua
+++ Scripts/DCS-BIOS/lib/L-39ZA.lua
@@ -13,6 +13,7 @@ local definePushButton = BIOS.util.definePushButton
 local definePotentiometer = BIOS.util.definePotentiometer
 local defineRotary = BIOS.util.defineRotary
 local defineTumb = BIOS.util.defineTumb
+local define3PosTumb = BIOS.util.define3PosTumb
 local defineToggleSwitch = BIOS.util.defineToggleSwitch
 local defineToggleSwitchToggleOnly = BIOS.util.defineToggleSwitchToggleOnly
 local defineString = BIOS.util.defineString
@@ -22,19 +23,33 @@ local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitc
 -- remove Arg# Pilot 600 / Instructor 610
 
 
------ 2do
+
 -----------------------------------L-39 GAUGES--------------------------------------------------
--- Barometric altimeter VD-20
-defineFloat("VD20", 52, {0, 1}, "instruments l-39","Altimeter KM")-- Kilometers gauge on VD-20
 
--- Radar altimeter RV-5
-defineFloat("RV_5_RALT", 58, {0, 1}, "instruments l-39","Radio Altimeter")
+defineFloat("FRONT_ALT_KM", 52, {0, 1}, "Gauges","FRONT VD-20 Altimeter KM")
+defineFloat("FRONT_ALT_M", 53, {0, 1}, "Gauges","FRONT VD-20 Altimeter M")
+defineFloat("FRONT_ALT_IND_KM", 54, {0, 1}, "Gauges","FRONT VD-20 Altimeter Indicator KM")
+defineFloat("FRONT_ALT_IND_M", 55, {0, 1}, "Gauges","FRONT VD-20 Altimeter Indicator M")
+defineFloat("FRONT_ALT_PRESS", 55, {0, 1}, "Gauges","FRONT VD-20 Altimeter Pressure")
+defineFloat("BACK_ALT_KM", 52, {0, 1}, "Gauges","BACK VD-20 Altimeter KM")
+defineFloat("BACK_ALT_M", 53, {0, 1}, "Gauges","BACK VD-20 Altimeter M")
+defineFloat("BACK_ALT_IND_KM", 54, {0, 1}, "Gauges","BACK VD-20 Altimeter Indicator KM")
+defineFloat("BACK_ALT_IND_M", 55, {0, 1}, "Gauges","BACK VD-20 Altimeter Indicator M")
+defineFloat("BACK_ALT_PRESS", 55, {0, 1}, "Gauges","BACK VD-20 Altimeter Pressure")
 
+defineFloat("FRONT_RALT", 58, {0.0,	0.086,	0.439,	0.878,	0.955}, "Gauges","FRONT Radar Altimeter")
+defineFloat("FRONT_RALT_DANGER", 59, {0.0,	0.094,	0.48,	0.998}, "Gauges","FRONT Radar Altimeter Danger Altitude")
+defineFloat("FRONT_RALT_WARNFLAG", 59, {0.0, 1.0}, "Gauges","FRONT Radar Altimeter Warning Flag")
+defineFloat("BACK_RALT", 58, {0.0,	0.086,	0.439,	0.878,	0.955}, "Gauges","BACK Radar Altimeter")
+defineFloat("BACK_RALT_DANGER", 59, {0.0,	0.094,	0.48,	0.998}, "Gauges","BACK Radar Altimeter Danger Altitude")
+defineFloat("BACK_RALT_WARNFLAG", 59, {0.0, 1.0}, "Gauges","BACK Radar Altimeter Warning Flag")
+
+
+----------------------------------------------------------------------------------------------------------------------------- 2do
 -- VARIOMETER
 defineFloat("Variometer", 74, {-1,1}, "instruments l-39","Variometer")
 defineFloat("Variometer_turn", 75, {-1, 1}, "instruments l-39","Roll indication variometre")
 defineFloat("Variometer_sideslip", 76, {-1, 1},"instruments l-39","Sideslip Variometer")
-
 -- ARTI HORIZ (KPP)
 defineFloat("KPP_1273K_roll", 38, {-1, 1}, "instruments l-39","KPP Roll")
 defineFloat("KPP_1273_pitch", 31, {-1, 1}, "instruments l-39","KPP Pitch")
@@ -46,7 +61,6 @@ defineFloat("KPP_Localizer_Beacon", 37, {0, 1}, "instruments l-39","LOC KPP")-- 
 -- defineFloat("KPP_Arretir", 29, {0.0, 1.0}, "instruments l-39"," Arretir KPP") (need to verify)
 defineFloat("KPP_SDU_Roll_Fwd", 32, {-1, 1}, "instruments l-39","SDU Roll KPP")-- SDU roll indication on KPP
 defineFloat("KPP_SDU_Pitch_Fwd", 33, {-1, 1}, "instruments l-39","SDU Pitch KPP")-- SDU pitch indicator on KPP
-
 -- NPP (HSI)
 defineFloat("HSI_heading", 41, {0, 1}, "instruments l-39","Heading indication")-- heading indication
 defineFloat("HSI_commanded_course_needle", 42, {1.0, 0.0}, "instruments l-39","Commanded course Needle")
@@ -55,61 +69,55 @@ defineFloat("HSI_Course_Deviation_Bar", 47, {-0.8, 0.8}, "instruments l-39","LOC
 defineFloat("HSI_Alt_Deviation_Bar", 45, {-0.8, 0.8}, "instruments l-39","GLIDE")
 defineFloat("HSI_Glide_Beacon", 46, {0, 1}, "instruments l-39","GLIDE HSI FLAG")-- GLIDE beacon
 defineFloat("HSI_Localizer_Beacon", 44, {0, 1}, "instruments l-39","LOC HSI FLAG")-- LOC beacon
-
 --RSBN
 defineFloat("RSBN_Range_1", 64, {0, 1}, "instruments l-39","Range RSBN 1KM")-- range indication by 1 KM
 defineFloat("RSBN_Range_10", 65, {0, 1}, "instruments l-39","Range RSBN 10KM")-- range indication by 10 KM
 defineFloat("RSBN_Range_100", 66, {0, 1}, "instruments l-39","Range RSBN 100KM")-- range indication by 100 KM
-
 --AIRSPEED AND MACH
 defineFloat("IAS", 49, {0, 1}, "instruments l-39","V indicated")-- indicated air speed
 defineFloat("TAS", 50, {0, 1}, "instruments l-39","V true")-- true air speed
 defineFloat("MACH", 51, {0, 1}, "instruments l-39","Number of MACH")-- number of Mach
-
 -- RKL-41
 defineFloat("RKL_41_needle", 77, {0, 1}, "instruments l-39","RKL Indicator")
-
 -- electric interface
 defineFloat("Voltmeter", 92, {0,	1}, "instruments l-39","VOLTMETER")
 defineFloat("Ampermeter", 93, {0,	1}, "instruments l-39","AMPERMETER")
-
 -- oxygen interface
 defineFloat("FwdOxygenPressure", 301, {0, 1}, "instruments l-39","Oxygene pressure")-- oxygene pressure indicator
 defineFloat("FwdFlowBlinker", 302, {0,	1}, "instruments l-39","Breathing blikers")-- breathing blinkers
-
 -- accelerometer
 defineFloat("Acceleration", 86, {0,	1}, "instruments l-39","G indicator")-- G indicator
-
 -- Cockpit pressure
 defineFloat("CockpitAltFwd", 95, {0, 1}, "instruments l-39","Cockpit altitude")-- cabine altitude
 defineFloat("PressDiffFwd", 96, {0,	1}, "instruments l-39","Differantial cockpit altitude")-- differantial cabine pressure
-
 ---Hydro Pressure
 defineFloat("MainHydro_PRESS", 198, {0, 1}, "instruments l-39","Main hydrolic pressure")-- main hydrolic system pressure indicator
 defineFloat("AuxHydro_PRESS", 200, {0, 1}, "instruments l-39","Emergency hydrolic pressure")-- aux hydrolic system pressure indicator
 defineFloat("BrakeLMainHydro_PRESS", 98, {0, 1}, "instruments l-39","Left brake pressure")-- main hydrolic left brake pressure indicator
 defineFloat("BrakeRMainHydro_PRESS", 99, {0, 1}, "instruments l-39","Right brake pressure")-- main hydrolic right brake pressure indicator
 defineFloat("BrakeAuxHydro_PRESS", 100, {0, 1}, "instruments l-39","Emergency brakes pressure")-- aux hydrolic brakes system pressure indicator
-
 -- Fuel 
 defineFloat("Fuel_Quantity", 91, {0, 1}, "instruments l-39","Fuel Quantity")-- fuel quantity indicator
 defineFloat("Fuel_Press", 81, {0, 1}, "instruments l-39","Fuel pressure")-- fuel pressure indicator
-
 -- ENGINE
 defineFloat("Fan_RPM", 85, {0, 1}, "instruments l-39","Low pressure compressor RPM")-- low pressure compressor rpm indicator
 defineFloat("Compressor_RPM", 84, {0, 1}, "instruments l-39","High pressure compressor RPM")-- high pressure compressor rpm indicator
 defineFloat("Engine_Temp", 90, {0, 1}, "instruments l-39","T4")-- turbine temperature indicator
 defineFloat("EngineVibration", 94, {0, 1}, "instruments l-39","Vibrations indicator")-- engine vibrations indicator
-
 -- OIL
 defineFloat("Oil_Temp", 83, {0, 1}, "instruments l-39","Oil temperature")-- oil temperature indicator
 defineFloat("Oil_Press", 82, {0, 1}, "instruments l-39","Oil pressure")-- oil pressure indicator
-  
 -- TRIM
 defineFloat("PitchTrimInd", 247, {-1, 1}, "instruments l-39","Pitch trim indicator") -- pitch trim indicator
+----------------------------------------------------------------------------------------------------------------------------- 2 do end
 
 ------------------------------------ L-39 Lights --------------------------------------------------------------------
+defineIndicatorLight("BACK_WP_DANGER_ALT_LAMP", 2, "Warning, Caution and IndicatorLights","BACK Dangerous Altitude Lamp (red)")
+defineIndicatorLight("BACK_RV5_DANGER_ALT_LAMP", 63, "Warning, Caution and IndicatorLights","BACK Dangerous Radar Altitude Lamp (yellow)")
+---defineIndicatorLight("FRONT_WP_DANGER_ALT_LAMP", XXXX, "Warning, Caution and IndicatorLights","FRONT Dangerous Altitude Lamp (red)")
+defineIndicatorLight("FRONT_RV5_DANGER_ALT_LAMP", 401, "Warning, Caution and IndicatorLights","FRONT Dangerous Radar Altitude Lamp (yellow)")
 
+----------------------------------------------------------------------------------------------------------------------------- 2do
 --- Elec syst lamps
 defineIndicatorLight("MRP_56_lamp", 18, "l-39 lamps","MRP 56")
 defineIndicatorLight("MainGen_lamp", 6, "l-39 lamps","Main generator")
@@ -117,15 +125,12 @@ defineIndicatorLight("ReservGen_lamp", 6, "l-39 lamps","Reserve generator")
 defineIndicatorLight("Inverter115_lamp", 12, "l-39 lamps","inverter 115v")
 defineIndicatorLight("Inverter363_lamp", 16, "l-39 lamps","inverter 3x36v")
 defineIndicatorLight("GroundPower_lamp", 316, "l-39 lamps","Ground power")
-
 --- Flaps
 defineIndicatorLight("FlapsUp_lamp", 278, "l-39 lamps","Flaps 0°")
 defineIndicatorLight("FlapsTO_lamp", 279, "l-39 lamps","Flaps 25°")
 defineIndicatorLight("FlapsDn_lamp", 280, "l-39lamps","Flaps 44°")
-
 --- Air Brakes
 defineIndicatorLight("AirBrakes_lamp", 117, "l-39 lamps","Airbrake")
-
 --- Gears
 defineIndicatorLight("GearDown_front_lamp", 113, "l-39 lamps","Front gear down")
 defineIndicatorLight("GearDown_left_lamp", 112, "l-39 lamps","Left gear down")
@@ -135,113 +140,86 @@ defineIndicatorLight("GearUp_left_lamp", 109, "l-39 lamps","Left gear up")
 defineIndicatorLight("GearUp_right_lamp", 111, "l-39 lamps","Right gear up")
 defineIndicatorLight("ExtendGears_lamp", 115, "l-39 lamps","Gears in Transition")
 defineIndicatorLight("DoorsOut_lamp", 116, "l-39 lamps","Doors out")
-
 --- RSBN (HSI)
 defineIndicatorLight("RSBN_Azim_Correction", 185, "l-39 lamps","RSBN correction AZIM")
 defineIndicatorLight("RSBN_Range_Correction", 186, "l-39 lamps","RSBN correction D")
-
 defineIndicatorLight("BreakdownFinished_lamp", 15, "l-39 lamps","BreakdownFinished_lamp")
-
 defineIndicatorLight("defineIndicatorLight", 123, "l-39 lamps","GMK tilt")
 defineIndicatorLight("defineIndicatorLight", 206, "l-39 lamps","GMK tilt PU-26")
 defineIndicatorLight("HSI_Accordance", 19, "l-39 lamps","HSI ok")
 ---RSBN_Azim_Correct = create_default_lamp(359, controllers.RSBN_Azimuth_Correction)
 ---RSBN_Range_Correct = create_default_lamp(362, controllers.RSBN_Range_Correction)
-
---- RV-5
-defineIndicatorLight("DangerAltitude_lamp", 2, "l-39 lamps","Dangerous Altitude")
-
 --- Fuel and turbostarter
 defineIndicatorLight("EmergFuelFwd_lamp", 27, "l-39 lamps","Emergency fuel system")
 defineIndicatorLight("TurboStarter_lamp", 23, "l-39 lamps","Turbo starter")
-
 defineIndicatorLight("FwdRemain150_lamp", 4, "l-39 lamps","Remain 150Kg")
 defineIndicatorLight("FwdDoNotStart_lamp", 7, "l-39 lamps","Do not start")
 defineIndicatorLight("FwdFuelFilter_lamp", 26, "l-39 lamps","Fuel Filter")
 defineIndicatorLight("L-39C_FwdWingTanks_lamp", 14, "l-39 lamps","L-39C Wing Tanks Empty")
-if L_39ZA then 
 defineIndicatorLight("L-39ZA_FwdWingTanks_lamp", 13, "l-39 lamps","L-39ZA Wing Tanks Empty")
-end
-
-
 --- Trim
 defineIndicatorLight("TrimmerRollNeutralFwd_lamp", 246, "l-39 lamps","Roll trim neutral")
-
 --- MACH
 defineIndicatorLight("MachMeterLampFwd", 3, "l-39 lamps","Over Mach")
-
 --- Canopy
 defineIndicatorLight("FwdCanopyNotClosed", 10, "l-39 lamps","Canopy not closed")
-
 --- Pitot
 defineIndicatorLight("LeftPitot_lamp", 556, "l-39 lamps","Pitot Left")
 defineIndicatorLight("RightPitot_lamp", 557, "l-39 lamps","Pitot Right")
-
 --- Vibrations
 defineIndicatorLight("FwdVibration_lamp", 8, "l-39 lamps","High vibrations")
-
 --- Fire
 defineIndicatorLight("FwdFire_lamp", 1, "l-39 lamps","FIRE")
-
 --- T°4 and Oil pressur
 defineIndicatorLight("FwdEngineTemperature700_lamp", 28, "l-39 lamps","T4 700")
 defineIndicatorLight("FwdEngineTemperature730_lamp", 24, "l-39 lamps","T4 730")
 defineIndicatorLight("FwdEngineMinOilPressure_lamp", 20, "l-39 lamps","press oil mini")
-
 --- Cockpit pressure & conditioning
 defineIndicatorLight("FwdCockpitPressure_lamp", 11, "l-39 lamps","Cockpit overpress")
 defineIndicatorLight("FwdConditioningClosed_lamp", 22, "l-39 lamps","Clim Off")
 defineIndicatorLight("EmergConditioning_lamp", 17, "l-39 lamps","Clim secours")
-
 --- Anti icing syst
 defineIndicatorLight("FwdDefrost_lamp", 25, "l-39 lamps","Anti icing on")
 defineIndicatorLight("FwdIce_lamp", 21, "l-39 lamps","Icing Warn")
 defineIndicatorLight("RIO_HeatingOk_lamp", 182, "l-39 lamps","RIO Heating On")
-
 --- Hydrolics
 defineIndicatorLight("FwdHydraulicPressureDrop_lamp", 5, "l-39 lamps","Hydro min press")
-
 --- Master caution
 defineIndicatorLight("FwdMasterDanger_lamp", 253, "l-39 lamps","SARTS")
-
 --- Radio control
 defineIndicatorLight("FwdRadioUnderControl_lamp", 562, "l-39 lamps","Control radio")
-
----2 do end
+----------------------------------------------------------------------------------------------------------------------------- 2 do end
 
 
 ------------------------------------ L-39 Clickable Controls --------------------------------------------------------------------
 
 -- Electric System
-defineToggleSwitch("BATTERY_SW",  4,3001, 141,"Electric System", "Accumulator Switch, ON/OFF")
-defineToggleSwitch("MAIN_GEN_SW",  4,3002, 142,"Electric System", "Main Generator Switch, ON/OFF")
-defineToggleSwitch("EMERG_GEN_SW",  4,3003, 143,"Electric System", "Emergency Generator Switch, ON/OFF")
-defineToggleSwitch("NET_SW",  4,3004, 502,"Electric System", "Net Switch, ON/OFF")
-defineToggleSwitch("EMERG_ENG_INST_PW",  4,3072, 169,"Electric System", "Emergency Engine Instruments Power Switch, ON/OFF")
+defineToggleSwitch("FRONT_BATTERY_SW",  4,3001, 141,"Electric System", "FRONT Accumulator Switch, ON/OFF")
+defineToggleSwitch("FRONT_MAIN_GEN_SW",  4,3002, 142,"Electric System", "FRONT Main Generator Switch, ON/OFF")
+defineToggleSwitch("FRONT_EMERG_GEN_SW",  4,3003, 143,"Electric System", "FRONT Emergency Generator Switch, ON/OFF")
+defineToggleSwitch("FRONT_NET_SW",  4,3004, 502,"Electric System", "FRONT Net Switch, ON/OFF")
+defineToggleSwitch("FRONT_EMERG_ENG_INST_PW",  4,3072, 169,"Electric System", "FRONT Emergency Engine Instruments Power Switch, ON/OFF")
 definePushButton("FRONT_TURBO_BTN",  4,3005, 315,"Electric System", "FRONT Turbo Button")
 defineToggleSwitch("FRONT_TURBO_SW_COVER",  4,3006, 314,"Electric System", "FRONT Turbo Button Cover, Open/Close")
 definePushButton("BACK_TURBO_BTN",  4,3007, 488,"Electric System", "BACK Turbo Button")
 defineToggleSwitch("BACK_TURBO_SW_COVER",  4,3008, 487,"Electric System", "BACK Turbo Button Cover, Open/Close")
 defineToggleSwitch("FRONT_STOP_TURBO_SW",  4,3009, 313,"Electric System", "FRONT Stop Turbo Switch, ON/OFF")
 defineToggleSwitch("FRONT_STOP_TURBO_COVER",  4,3010, 312,"Electric System", "FRONT Stop Turbo Switch Cover, Open/Close")
-
------ 2do
-defineToggleSwitch("FwdEngineSw",  4,3011, 142,"Electric System", "FwdEngineSw")
-defineToggleSwitch("FwdEngineSwCover",  4,3012, 142,"Electric System", "FwdEngineSwCover")
-defineToggleSwitch("AftEngineSw",  4,3013, 142,"Electric System", "AftEngineSw")
-defineToggleSwitch("AftEngineSwCover",  4,3014, 142,"Electric System", "AftEngineSwCover")
-defineToggleSwitch("FwdStopEngineSw",  4,3015, 142,"Electric System", "FwdStopEngineSw")
-defineToggleSwitch("FwdStopEngineSwCover",  4,3016, 142,"Electric System", "FwdStopEngineSwCover")
-defineToggleSwitch("AftStopEngineSw",  4,3017, 142,"Electric System", "AftStopEngineSw")
-defineToggleSwitch("AftStopEngineSwCover",  4,3018, 142,"Electric System", "AftStopEngineSwCover")
-defineToggleSwitch("EngineStartModeSw",  4,3019, 142,"Electric System", "EngineStartModeSw")
-defineToggleSwitch("EngineStartModeSwCover",  4,3020, 142,"Electric System", "EngineStartModeSwCover, ON/OFF")
-defineToggleSwitch("FwdEmergFuelSw",  4,3021, 142,"Electric System", "FwdEmergFuelSw")
-defineToggleSwitch("FwdEmergFuelSwCover",  4,3022, 142,"Electric System", "FwdEmergFuelSwCover")
-defineToggleSwitch("AftEmergFuelSw",  4,3023, 142,"Electric System", "AftEmergFuelSw")
-defineToggleSwitch("AftEmergFuelSwCover",  4,3024, 142,"AftEmergFuelSwCover")
----2 do end
-
+definePushButton("FRONT_ENGINE",  4,3011, 326,"Electric System", "FRONT Engine Button")
+defineToggleSwitch("FRONT_ENGINE_COVER",  4,3012, 325,"Electric System", "FRONT Engine Button Cover")
+definePushButton("BACK_ENGINE",  4,3013, 494,"Electric System", "BACK Engine Button")
+defineToggleSwitch("BACK_ENGINE_COVER",  4,3014, 493,"Electric System", "BACK Engine Button Cover")
+defineToggleSwitch("FRONT_STOP_ENGINE",  4,3015, 318,"Electric System", "FRONT Stop Engine Switch")
+defineToggleSwitch("FRONT_STOP_ENGINE_COVER",  4,3016, 317,"Electric System", "FRONT Stop Engine Switch Cover, Open/Close")
+defineToggleSwitch("BACK_STOP_ENGINE",  4,3017, 490,"Electric System", "BACK Stop Engine Switch")
+defineToggleSwitch("BACK_STOP_ENGINE_COVER",  4,3018, 489,"Electric System", "BACK Stop Engine Switch Cover, Open/Close")
+defineMultipositionSwitch("L39C_FRONT_ENG_START_MODE",  4,3019, 322, 3, 0.1,"Electric System", "L39C FRONT Engine Start Mode Switch")
+define3PosTumb("L39ZA_FRONT_ENG_START_MODE",  4,3019, 322,"Electric System", "L39ZA FRONT Engine Start Mode Switch")
+defineToggleSwitch("FRONT_ENG_STRT_MODE_COVER",  4,3020, 321,"Electric System", "FRONT Engine Start Mode Switch Cover, Open/Close")
+defineToggleSwitch("FRONT_EMERG_FUEL",  4,3021, 320,"Electric System", "FRONT Emergency Fuel Switch")
+defineToggleSwitch("FRONT_EMERG_FUEL_COVER",  4,3022, 319,"Electric System", "FRONT Emergency Fuel Switch Cover")
+defineToggleSwitch("BACK_EMERG_FUEL",  4,3023, 492,"Electric System", "BACK Emergency Fuel Switch")
+defineToggleSwitch("BACK_EMERG_FUEL_COVER",  4,3024, 491,"BACK Emergency Fuel Switch Cover")
 defineToggleSwitch("CB_ENGINE",  4,3025, 144,"Electric System", "CB Engine")
 defineToggleSwitch("CB_AGD_GMK",  4,3026, 145,"Electric System", "CB AGD-GMK")
 defineToggleSwitch("CB_INVERT1",  4,3027, 146,"Electric System", "CB Inverter 1 (AC 115V)")
@@ -285,7 +263,6 @@ defineToggleSwitch("CB_SEAT_INSTRUCTOR",  4,3064, 503,"Electric System", "CB Sea
 defineToggleSwitch("CB_SIGNAL_INSTRUCTOR",  4,3065, 504,"Electric System", "CB Signal Instructor")
 defineToggleSwitch("CB_WEAPON_INSTRUCTOR",  4,3066, 505,"Electric System", "CB Weapon Instructor")
 defineToggleSwitch("CB_GROUND_INTCOM",  4,3067, 512,"Electric System", "CB Ground Intercom")
-
 definePushButton("PITOT_HEAT_ON_L",  4,3068, 294,"Electric System", "Standby (Left) Pitot Tube Heating ON Button")
 definePushButton("PITOT_HEAT_OFF_L",  4,3069, 295,"Electric System", "Standby (Left) Pitot Tube Heating OFF Button")
 definePushButton("PITOT_HEAT_ON_R",  4,3070, 292,"Electric System", "Main (Right) Pitot Tube Heating ON Button")
