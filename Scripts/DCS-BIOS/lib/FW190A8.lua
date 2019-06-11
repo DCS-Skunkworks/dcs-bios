d2eb9cac0d9c0795d5057b256122b8d6695de999
Sven <warlord12@freenet.de>
2019-06-11 19:11:42 +0200

    Add Fw 190 A-8

---
+++ Scripts/DCS-BIOS/lib/FW190A8.lua
@@ -0,0 +1,36 @@
+BIOS.protocol.beginModule("FW-190A8", 0x3800)
+BIOS.protocol.setExportModuleAircrafts({"FW-190A8"})
+
+local documentation = moduleBeingDefined.documentation
+
+local document = BIOS.util.document  
+
+local parse_indication = BIOS.util.parse_indication
+
+local defineFloat = BIOS.util.defineFloat
+local defineIndicatorLight = BIOS.util.defineIndicatorLight
+local definePushButton = BIOS.util.definePushButton
+local definePotentiometer = BIOS.util.definePotentiometer
+local defineRotary = BIOS.util.defineRotary
+local defineSetCommandTumb = BIOS.util.defineSetCommandTumb
+local defineTumb = BIOS.util.defineTumb
+local defineToggleSwitch = BIOS.util.defineToggleSwitch
+local defineToggleSwitchToggleOnly = BIOS.util.defineToggleSwitchToggleOnly
+local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
+local defineFixedStepInput = BIOS.util.defineFixedStepInput
+local defineVariableStepTumb = BIOS.util.defineVariableStepTumb
+local defineString = BIOS.util.defineString
+local defineRockerSwitch = BIOS.util.defineRockerSwitch
+local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
+local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
+
+
+--Externals
+defineIntegerFromGetter("EXT_POSITION_LIGHT_LEFT", function()
+	if LoGetAircraftDrawArgumentValue(190) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Left Position Light (red)")
+defineIntegerFromGetter("EXT_POSITION_LIGHT_RIGHT", function()
+	if LoGetAircraftDrawArgumentValue(191) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Right Position Light (green)")
+
+BIOS.protocol.endModule()
