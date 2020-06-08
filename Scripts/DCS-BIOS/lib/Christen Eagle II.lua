29c93dd82ecce159f21d32e5a53b0f63478e341a
Sven <warlord12@freenet.de>
2020-06-08 22:23:45 +0200

    CE - add canopy position

--- Scripts/DCS-BIOS/lib/Christen Eagle II.lua
+++ Scripts/DCS-BIOS/lib/Christen Eagle II.lua
@@ -90,6 +90,7 @@ defineFloat("RPM", 318, {0.0, 0.120, 0.271, 0.416, 0.557, 0.696, 0.845, 1.0}, "G
 defineFloat("EGT", 319, {0.0, 0.260, 0.481, 0.661, 0.820, 0.979}, "Gauges", "EGT")
 defineFloat("EGT_MAX", 320, {0.0, 0.289, 0.505, 0.676, 0.838, 0.992}, "Gauges", "EGT Max")
 defineFloat("VAMETER", 376, {0.104, 0.260, 0.395, 0.564, 0.704, 0.856, 0.996}, "Gauges", "Volt-Ampermeter Gauge")
+defineFloat("CANOPY_POS", 400, {0, 1}, "Canopy", "Canopy Position")
 
 --Externals
 defineIntegerFromGetter("EXT_POSITION_LIGHT_LEFT", function()
