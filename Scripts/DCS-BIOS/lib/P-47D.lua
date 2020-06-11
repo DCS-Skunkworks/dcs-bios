0fbb0bce236afd7cab272b41cfdfedc9dd14c009
Sven <warlord12@freenet.de>
2020-06-11 16:21:56 +0200

    P-47 - cleanup

--- Scripts/DCS-BIOS/lib/P-47D.lua
+++ Scripts/DCS-BIOS/lib/P-47D.lua
@@ -14,15 +14,8 @@ local define3PosTumb = BIOS.util.define3PosTumb
 local define3Pos2CommandSwitchWW2 = BIOS.util.define3Pos2CommandSwitchWW2
 local definePotentiometer = BIOS.util.definePotentiometer
 local defineRotary = BIOS.util.defineRotary
-local defineSetCommandTumb = BIOS.util.defineSetCommandTumb
 local defineTumb = BIOS.util.defineTumb
 local defineToggleSwitch = BIOS.util.defineToggleSwitch
-local defineToggleSwitchToggleOnly = BIOS.util.defineToggleSwitchToggleOnly
-local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
-local defineFixedStepInput = BIOS.util.defineFixedStepInput
-local defineVariableStepTumb = BIOS.util.defineVariableStepTumb
-local defineString = BIOS.util.defineString
-local defineRockerSwitch = BIOS.util.defineRockerSwitch
 local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
 local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
 
