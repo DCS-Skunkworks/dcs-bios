a7e3e816f3b94f69c3fd97199dfeafccf20c6b98
Sven <warlord12@freenet.de>
2020-06-11 16:16:49 +0200

    P-47 finished

--- Scripts/DCS-BIOS/lib/P-47D.lua
+++ Scripts/DCS-BIOS/lib/P-47D.lua
@@ -100,8 +100,8 @@ defineToggleSwitch("CANOPY_JETT_SW", 2, 3020, 157, "Canopy Controls", "Cockpit J
 definePushButton("HATCH_KNB", 2, 3016, 154, "Canopy Controls", "Canopy Hatch Knobs")
 
 --Main Panel
-definePushButton("CLK_PINION_PULL", 5, 3006, 8, "Main Panel", "Clock Pull out.")  
-defineRotary("CLK_PINION", 5, 3004, 7, "Main Panel", "Clock Winding/Adjustment ") 
+definePushButton("CLK_PINION_PULL", 5, 3006, 8, "Main Panel", "Clock Pullout")  
+defineRotary("CLK_PINION", 5, 3004, 7, "Main Panel", "Clock Winding/Adjustment") 
 define3PosTumb("STARTER_SW", 3, 3021, 63, "Main Panel", "Starter Switch")
 definePotentiometer("PRIMER_TURN", 3, 3022, 65, {0,1}, "Main Panel", "Primer Pump Turn")
 definePushButton("PRIMER_PULL", 3, 3025, 64, "Main Panel", "Primer Pump Pull")
@@ -140,6 +140,11 @@ defineToggleSwitch("RECOV_FLAP_SW_C", 13, 3036, 143, "Port Wall", "Recovery Flap
 defineToggleSwitch("RECOV_FLAP_SW", 13, 3039, 144, "Port Wall", "Recovery Flaps Switch")
 define3PosTumb("INTCOOL_FLP_SW", 3, 3041, 87, "Port Wall", "Intercooler Flap Switch")
 define3PosTumb("OILCOOL_FLP_SW", 3, 3044, 88, "Port Wall", "Oil Cooler Flap Switch")
+defineToggleSwitch("GUNS_SW_C", 13, 3003, 67, "Port Wall", "Guns Switch Cover")
+define3PosTumb("GUNS_SW", 13, 3005, 68, "Port Wall", "Guns Switch")
+defineTumb("FLAPS_LVR", 2, 3029, 94, 0.5, {0, 1}, nil, false, "Port Wall", "Landing Flaps Lever")
+definePotentiometer("LH_C5_CAP", 17, 3004, 166, {0, 1}, "Port Wall", "LH C5 Assy. Cover")
+defineTumb("GEAR_LVR", 2, 3034, 153, 2, {-1, 1}, nil, false, "Port Wall", "Gear Lever")
 
 definePushButton("VHF_PTT_BTN", 21, 3099, 44, "Throttle Quadrant", "Radio PTT Button")
 definePushButton("NOS_BTN", 3, 3001, 40, "Throttle Quadrant", "NOS Push Button")
@@ -152,6 +157,31 @@ definePotentiometer("MIX_LVR", 3, 3012, 48, {0,1}, "Throttle Quadrant", "Mixture
 definePotentiometer("THROTTLE_FRIC", 3, 3018, 57, {0,1}, "Throttle Quadrant", "Throttle Frictioner")
 
 defineMultipositionSwitch("PROP_CONTR_SW", 3, 3029, 72, 4, 0.1,"Propellor Control" ,"Propellor Control Switch")
+
+definePotentiometer("TRIM_WH_AILERON", 2, 3001, 91, {-1,1}, "Trim Control", "Aileron Trim Wheel")
+definePotentiometer("TRIM_WH_ELEVATOR", 2, 3004, 92, {-1,1}, "Trim Control", "Elevator Trim Wheel")
+definePotentiometer("TRIM_WH_RUDDER", 2, 3007, 93, {-1,1}, "Trim Control", "Rudder Trim Wheel")
+
+definePushButton("PROP_CB", 7, 3085, 73, "Circuit Brakers", "Propellor Control CB")
+definePushButton("MSB_CB_GUN_LI", 7, 3051, 204, "Circuit Brakers", "LH Inboard Guns CB")
+definePushButton("MSB_CB_GUN_LO", 7, 3053, 205, "Circuit Brakers", "LH Outboard Guns CB")
+definePushButton("MSB_CB_GUN_RI", 7, 3055, 206, "Circuit Brakers", "LH Inboard Guns CB")
+definePushButton("MSB_CB_GUN_RO", 7, 3057, 207, "Circuit Brakers", "LH Outboard Guns CB")
+definePushButton("MSB_CB_FPUMP", 7, 3059, 208, "Circuit Brakers", "Fuel Pumps CB")
+definePushButton("MSB_CB_OILCLR", 7, 3061, 209, "Circuit Brakers", "Oil Cooler CB")
+definePushButton("MSB_CB_INTRCLR", 7, 3063, 210, "Circuit Brakers", "Intercooler CB")
+definePushButton("MSB_CB_GUNRELAY", 7, 3065, 211, "Circuit Brakers", "Gun Relay and Gun Camera Guns CB")
+definePushButton("MSB_CB_GUNSIGHT", 7, 3067, 212, "Circuit Brakers", "Gun Sight, Camera Heat & Water Injection CB")
+definePushButton("MSB_CB_CANOPY", 7, 3069, 213, "Circuit Brakers", "Canopy Motor CB")
+definePushButton("MSB_CB_LLIGHT", 7, 3071, 214, "Circuit Brakers", "Landing Light CB")
+definePushButton("MSB_CB_WARN", 7, 3073, 215, "Circuit Brakers", "Warn. Instruments CB")
+definePushButton("MSB_CB_CLIGHT", 7, 3075, 216, "Circuit Brakers", "Internal Lighting CB")
+definePushButton("MSB_CB_PITOT", 7, 3077, 217, "Circuit Brakers", "Pitot & Starter CB")
+definePushButton("MSB_CB_NLIGHT", 7, 3079, 218, "Circuit Brakers", "Position Lights CB")
+definePushButton("MSB_CB_RLIGHT", 7, 3081, 219, "Circuit Brakers", "Recognition Lights CB")
+definePushButton("MSB_CB_RADIO", 7, 3083, 220, "Circuit Brakers", "Radio CB")
+definePushButton("MSB_CB_BOMBS", 7, 3087, 158, "Circuit Brakers", "Drop Ordinance CB")
+definePushButton("MSB_CB_CRFLAPS", 7, 3089, 159, "Circuit Brakers", "Recovery Flaps CB")
 
 definePotentiometer("MSB_GUNSIGHT", 7, 3001, 194, {0,1}, "Main Switch Box", "Gun Sight Rheostat")
 definePotentiometer("MSB_COMPASS", 7, 3004, 195, {0,1}, "Main Switch Box", "Compass Light Rheostat")
@@ -168,6 +198,14 @@ define3PosTumb("MSB_TEST1", 7, 3030, 202, "Main Switch Box", "LG/FL Test Switch"
 define3PosTumb("MSB_TEST2", 7, 3031, 203, "Main Switch Box", "OXY/TRB Test Switch")
 
 --Starboard Wall
+definePushButton("IFF_DESTR_1", 14, 3087, 185, "Starboard Wall", "Secret Radio Destruction Box 1")
+definePushButton("IFF_DESTR_2", 14, 3089, 186, "Starboard Wall", "Secret Radio Destruction Box 2")
+definePotentiometer("RH_C5_CAP", 18, 3004, 168, {0, 1}, "Starboard Wall", "RH C5 Assy. Cover")
+defineToggleSwitch("TAIL_LOCK", 2, 3042, 101, "Starboard Wall", "Tail Wheel Lock Lever")
+
+definePotentiometer("OXY_BYPASS", 4, 3005, 130, {0, 1}, "Oxygen", "Oxygen Emergency By-pass") 
+defineToggleSwitch("OXY_AUTOMIX", 4, 3003, 131, "Oxygen", "Oxygen Auto-Mix On-Off")
+
 definePushButton("RLB_KEY", 7, 3035, 96, "Recognition Lights", "Recognition Lights KEY Button")
 define3PosTumb("RLB_WHITE", 7, 3036, 97, "Recognition Lights", "Recognition Lights Switch White")
 define3PosTumb("RLB_RED", 7, 3039, 98, "Recognition Lights", "Recognition Lights Switch Red")
@@ -187,6 +225,18 @@ definePotentiometer("RCTRL_VOL", 21, 3015, 77, {0,1}, "Radio", "Radio Volume")
 definePotentiometer("DETROLA_FREQU_SEL", 22, 3001, 137, {0,1}, "Detrola Receiver", "Detrola Frequency Selector")
 definePotentiometer("DETROLA_VOL", 22, 3002, 138, {0,1}, "Detrola Receiver", "Detrola Volume")
 
+--Deck
+defineTumb("MAIN_FUEL_SEL", 3, 3047, 85,  0.25, {0, 1}, nil, true, "Deck" ,"Main Fuel Tank Selector")
+defineMultipositionSwitch("DROP_TANK_SEL", 3, 3050, 86, 3, 0.5, "Deck" ,"Droptank Selector")        
+defineToggleSwitch("AIRFILTER", 3, 3033, 114, "Deck", "Air Filter By-Pass Lever")
+defineToggleSwitch("HYDR_HP", 2, 3052, 160, "Deck", "Hydraulic System Hand Pump")
+definePotentiometer("CARBAIR", 3, 3054, 161, {0, 1}, "Deck", "Carburettor Heater Lever")
+
+--Gun Sight
+definePotentiometer("WING_SPAN_SEL", 6, 3001, 35, {0, 1}, "Gun Sight", "Wing Span Selector")
+definePotentiometer("TARGET_RANGE_SEL", 6, 3020, 36, {0, 1}, "Gun Sight", "Target Range Selector")
+defineToggleSwitch("RET_MASK_LVR", 6, 3006, 37, "Gun Sight", "Fixed Reticle Mask Lever")
+
 --Warning, Caution and IndicatorLights
 defineIndicatorLight("TURB_OVER_SP_L", 59, "Warning, Caution and IndicatorLights","Turbine Overspeed Light (red)")
 defineIndicatorLight("FUEL_LVL_WARN_L", 69, "Warning, Caution and IndicatorLights","Fuel Level Warning Light (yellow)")
@@ -200,7 +250,7 @@ defineIndicatorLight("VHF_RADIO_B_L", 121, "Warning, Caution and IndicatorLights
 defineIndicatorLight("VHF_RADIO_C_L", 122, "Warning, Caution and IndicatorLights", "VHF Radio C Light (green)")
 defineIndicatorLight("VHF_RADIO_D_L", 123, "Warning, Caution and IndicatorLights", "VHF Radio D Light (green)")
 defineIndicatorLight("VHF_RADIO_TX_L", 124, "Warning, Caution and IndicatorLights", "VHF Radio TX Light (white)")
---defineIndicatorLightMulti1("L_UV_LAMP_RD", 167, "Warning, Caution and IndicatorLights","Left UV Instruments Lamp (red)")------------------------WIP
+--defineIndicatorLightMulti1("L_UV_LAMP_RD", 167, "Warning, Caution and IndicatorLights","Left UV Instruments Lamp (red)")------------------------WIP C5lamp#= 173/174
 defineIndicatorLightMulti2("L_UV_LAMP_BL", 167, "Warning, Caution and IndicatorLights","Left UV Instruments Lamp (blue)")
 --defineIndicatorLightMulti1("R_UV_LAMP_RD", 169, "Warning, Caution and IndicatorLights","Right UV Instruments Lamp (red)") ----------------------WIP
 defineIndicatorLightMulti2("R_UV_LAMP_BL", 169, "Warning, Caution and IndicatorLights","Right UV Instruments Lamp (blue)")
