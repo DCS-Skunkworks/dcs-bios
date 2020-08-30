71a04338f86c41e82756345d0103b93cd345e26f
Sven <warlord12@freenet.de>
2020-08-30 17:57:28 +0200

    Commondata - nil fix

--- Scripts/DCS-BIOS/lib/CommonData.lua
+++ Scripts/DCS-BIOS/lib/CommonData.lua
@@ -30,14 +30,12 @@ moduleBeingDefined.exportHooks[#moduleBeingDefined.exportHooks+1] = function()
 		if lat < 0 then lat = 0 - lat 
 						latDir = "S"
 		else lat = lat 
-			 latDir = "N" end
-    if latDir == nil then latDir = "N" end			 
+			 latDir = "N" end		 
 	local lon = selfData.LatLongAlt.Long
 		if lon < 0 then lon = 0 - lon 
 						lonDir	= "W"
 		else lon = lon 
-		     lonDir = "E" end
-	if lonDir == nil then lonDir = "E" end				 
+		     lonDir = "E" end			 
 
 	latDeg = math.floor(lat)
 	lat1 = (lat - latDeg) * 60 -- convert to seconds
@@ -63,7 +61,7 @@ defineString("DCS_BIOS", getVersion, 6,  "Metadata" , "DCS Bios Version")
 
 defineString("PILOTNAME", function() return playerName end, 24, "Metadata", "Pilot Name")
 
-defineString("LAT_Z_DIR", function() return latDir end, 1, "Position", "Latitude Direction")
+defineString("LAT_Z_DIR", function() return latDir or "N" end, 1, "Position", "Latitude Direction")
 defineIntegerFromGetter("LAT_DEG", function() return latDeg end, 90, "Position", "Latitude Degrees")
 defineIntegerFromGetter("LAT_SEC", function() return latSec end, 59, "Position", "Latitude Seconds")
 defineIntegerFromGetter("LAT_SEC_FRAC", function()
@@ -82,7 +80,7 @@ defineIntegerFromGetter("IAS_US_INT", function()
 	return iasDisp * 1.94384449
 end, 65535, "Speed", "Indicated Airspeed KNT (Int)")
 
-defineString("LON_Z_DIR", function() return lonDir end, 1, "Position", "Longitude Direction")
+defineString("LON_Z_DIR", function() return lonDir or "E" end, 1, "Position", "Longitude Direction")
 defineIntegerFromGetter("LON_DEG", function() return lonDeg end, 180, "Position", "Longitude Degrees")
 defineIntegerFromGetter("LON_SEC", function() return lonSec end, 59, "Position", "Longitude Seconds")
 defineIntegerFromGetter("LON_SEC_FRAC", function()
