3bab3a1934c2d4d608368abd3b187932c49f6a7c
Sven <warlord12@freenet.de>
2021-01-16 22:33:41 +0100

    Edge 540 - finished

--- Scripts/DCS-BIOS/lib/Edge540.lua
+++ Scripts/DCS-BIOS/lib/Edge540.lua
@@ -18,20 +18,15 @@ local defineFloat = BIOS.util.defineFloat
 local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
 
 defineRotary("ALT_SET", 2, 3001, 6, "Main Panel" , "Altimeter Setting") 
-defineToggleSwitch("MASTER_SW", 4, 3001, 18, "Main Panel", "Master Switch")--3051
-defineToggleSwitch("BOOST_SW", 4, 3002, 19, "Main Panel", "Electric Fuel Pump")--3052
-defineToggleSwitch("CANOPY", 6, 71, 0, "Main Panel", "Canopy")
+defineToggleSwitch("MASTER_SW", 4, 3051, 18, "Main Panel", "Master Switch")
+defineToggleSwitch("BOOST_SW", 4, 3052, 19, "Main Panel", "Electric Fuel Pump")
+defineToggleSwitch("CANOPY", 6, 71, 0, "Main Panel", "Canopy Open/Close")
 definePushButton("UAV_BTN",	2, 3007, 0, "Main Panel", "UAV Mode")
 definePushButton("LED_BTN", 2, 3008, 0, "Main Panel", "Racing LED Indicator Speed/G Toggle")
-definePushButton("CUSTOM_5_BTN", 2, 3006, 0, "Main Panel", "Hide Aersti Chart")
 definePushButton("STOPWATCH_BTN", 7, 3005, 0, "Main Panel", "Stopwatch Start/Stop/Reset")
-
-definePotentiometer("PROP_MIX", 3, 3003, 17, {0, 1}, "Engine" , "Mixture Lever")
-defineTumb("FUEL_SEL", 4, 3053, 21, 0.5, {0.0, 1.0}, nil, false, "Engine", "Fuel Selector")--?
-defineTumb("MAG", 4, 3050, 14, 0.25, {0.0, 1.0}, nil, false, "Engine", "Magnetos")
-
-defineTumb("PILOT_SEL", 8, 3013, 0, 0.1, {0.0, 0.6}, nil, false, "Custom", "Select Pilot")
-defineTumb("FLOOD_L_COL", 8, 3023, 0, 0.1, {0.0, 0.5}, nil, false, "Custom", "Flood Light Color")
+definePotentiometer("PROP_MIX", 3, 3003, 17, {0, 1}, "Main Panel" , "Mixture Lever")
+defineTumb("FUEL_SEL", 3, 3053, 21, 0.5, {0.0, 1.0}, nil, false, "Main Panel", "Fuel Selector")
+defineTumb("MAG", 3, 3050, 14, 0.25, {0.0, 1.0}, nil, false, "Main Panel", "Magnetos")
 
 definePushButton("PAGE_1BTN_NAV", 8, 3015, 34, "Main Display", "Display Button 1 NAV Page")
 definePushButton("PAGE_2BTN_SYS", 8, 3017, 32, "Main Display", "Display Button 2 System Page")
@@ -39,19 +34,23 @@ definePushButton("PAGE_3BTN_MP3", 8, 3018, 31, "Main Display", "Display Button 3
 definePushButton("PAGE_4BTN_CONF", 8, 3001, 30, "Main Display", "Display Button 4 Configuration Page")
 
 --System Page Buttons
-definePushButton("NAV_BTN", 4, 3001, 0, "Main Display", "System Page Navigation Lights")
-definePushButton("FLOOD_BTN", 4, 3012, 0, "Main Display", "System Page Flood Lights")
-definePushButton("GAUGE_BTN", 4, 3013, 42, "Main Display", "System Page Gauge Lights")
-definePushButton("WALL_BTN", 8, 3019, 0, "Main Display", "System Page Wallpaper")
+definePushButton("SYS_NAV_BTN", 4, 3001, 0, "System Page", "System Page - Navigation Lights")
+definePushButton("SYS_FLOOD_BTN", 4, 3012, 0, "System Page", "System Page - Flood Lights")
+definePushButton("SYS_GAUGE_BTN", 4, 3013, 42, "System Page", "System Page - Gauge Lights")
+definePushButton("SYS_WALL_BTN", 8, 3019, 0, "System Page", "System Page - Wallpaper")
+defineTumb("SYS_FLOOD_L_COL", 8, 3023, 0, 0.1, {0.0, 0.5}, nil, false, "System Page", "System Page - Flood Light Color")
 
 --MP3 Player
-definePushButton("MP3_PLAY", 9, 3030, 0, "Main Display", "Mp3 Play")
-definePushButton("MP3_NEXT", 9, 3031, 0, "Main Display", "Mp3 Next Track")
-definePushButton("MP3_PREV", 9, 3032, 0, "Main Display", "Mp3 Previous Track")
+definePushButton("MP3_PLAY", 9, 3030, 0, "Mp3 Page", "Mp3 Play")
+definePushButton("MP3_NEXT", 9, 3031, 0, "Mp3 Page", "Mp3 Next Track")
+definePushButton("MP3_PREV", 9, 3032, 0, "Mp3 Page", "Mp3 Previous Track")
 
-defineToggleSwitch("CUSTOM_1_BTN", 8, 3010, 0, "Custom", "Attach/Remove Winglets")
-defineToggleSwitch("CUSTOM_2_BTN", 8, 3011, 0, "Custom", "Attach/Remove Spades")
-defineToggleSwitch("CUSTOM_3_BTN", 8, 3012, 0, "Custom", "Attach/Remove Lights")
+--Configuration Page Buttons
+defineToggleSwitch("CONF_CUSTOM_1_BTN", 8, 3010, 0, "Config Page", "Config Page - Attach/Remove Winglets")
+defineToggleSwitch("CONF_CUSTOM_2_BTN", 8, 3011, 0, "Config Page", "Config Page - Attach/Remove Spades")
+defineToggleSwitch("CONF_CUSTOM_3_BTN", 8, 3012, 0, "Config Page", "Config Page - Attach/Remove Lights")
+defineTumb("CONF_PILOT_SEL", 8, 3013, 0, 0.1, {0.0, 0.6}, nil, false, "Config Page", "Config Page - Select Pilot")
+definePushButton("CONF_CUSTOM_5_BTN", 2, 3006, 0, "Config Page", "Config Page - Hide Aersti Chart")
 
 defineToggleSwitch("SMK_ARM", 1, 3025, 27, "Smoke", "Smoke Arm")
 
@@ -61,7 +60,7 @@ defineIndicatorLight("COCKPIT_L", 41, "Warning, Caution and IndicatorLights","Co
 defineIndicatorLight("INSTR_L", 42, "Warning, Caution and IndicatorLights","Instrument Lights (white)")
 
 --Gauges
-defineFloat("CANOPY_POS", 181, {0, 1}, "Canopy", "Canopy Position")
+defineFloat("CANOPY_POS", 181, {0, 1}, "Gauges", "Canopy Position")
 defineFloat("AIRSPEED", 5, {0, 1}, "Gauges", "Airspeed in Knots Gauge")
 defineFloat("FUEL_INT", 22, {0, 1}, "Gauges", "Internal Tanks Fuel Gauge")
 defineFloat("FUEL_WING", 23, {0, 1}, "Gauges", "Wing Tanks Fuel Gauge")
