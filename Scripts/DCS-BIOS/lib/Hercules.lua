687d4a75a7855e17df599ffb34b5f8e443203052
Sven <warlord12@freenet.de>
2021-02-09 15:33:02 +0100

    A-4E-C - v2.0.0-beta-1 updates / hercules - device numbers change (still not working)

--- Scripts/DCS-BIOS/lib/Hercules.lua
+++ Scripts/DCS-BIOS/lib/Hercules.lua
@@ -19,32 +19,34 @@ local defineMultipositionSwitch = BIOS.util.defineMultipositionSwitch
 local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
 
 --ICS_PANEL
-defineToggleSwitch("P_ICS_VOL_BTN", 1, 13039, 1431, "ICS Panel", "PILOT ICS Volume (Pull)")
-definePotentiometer("P_ICS_VOL_KNB", 1, 13038, 1430, {-0.5, 0.5}, "ICS Panel", "PILOT ICS Volume (Rotate)")
-defineToggleSwitch("CP_ICS_VOL_BTN", 1, 13040, 1433, "ICS Panel", "COPILOT ICS Volume (Pull)")
-definePotentiometer("CP_ICS_VOL_KNB", 1, 13041, 1432, {-0.5, 0.5}, "ICS Panel", "COPILOT ICS Volume (Rotate)")
-definePotentiometer("P_ICS_UN_KNB", 1, 13041, 1434, {-0.5, 0.5}, "ICS Panel", "PILOT ICS Undefined Knob")
-definePotentiometer("CP_ICS_UN_KNB", 1, 13042, 1435, {-0.5, 0.5}, "ICS Panel", "COPILOT ICS Undefined Knob")
+defineToggleSwitch("P_ICS_VOL_BTN", 14, 13039, 1431, "ICS Panel", "PILOT ICS Volume (Pull)")
+definePotentiometer("P_ICS_VOL_KNB", 14, 13038, 1430, {-0.5, 0.5}, "ICS Panel", "PILOT ICS Volume (Rotate)")
+defineToggleSwitch("CP_ICS_VOL_BTN", 14, 13040, 1433, "ICS Panel", "COPILOT ICS Volume (Pull)")
+definePotentiometer("CP_ICS_VOL_KNB", 14, 13041, 1432, {-0.5, 0.5}, "ICS Panel", "COPILOT ICS Volume (Rotate)")
+definePotentiometer("P_ICS_UN_KNB", 14, 13041, 1434, {-0.5, 0.5}, "ICS Panel", "PILOT ICS Undefined Knob")
+definePotentiometer("CP_ICS_UN_KNB", 14, 13042, 1435, {-0.5, 0.5}, "ICS Panel", "COPILOT ICS Undefined Knob")
 
 -- ELECTRICAL_PANEL
-defineToggleSwitch("BATTERY_SW", 1, 13046, 1000, "Electric Panel", "Battery Switch")
-define3PosTumb("EXT_PW_SW", 1, 13047, 1001, "Electric Panel", "External Power/OFF/APU Switch")
-defineToggleSwitch("GEN1_SW", 1, 13048, 1003, "Electric Panel", "Generator 1 Switch")
-defineToggleSwitch("GEN2_SW", 1, 13049, 1004, "Electric Panel", "Generator 2 Switch")
-defineToggleSwitch("GEN3_SW", 1, 13050, 1005, "Electric Panel", "Generator 3 Switch")
-defineToggleSwitch("GEN4_SW", 1, 13051, 1006, "Electric Panel", "Generator 4 Switch")
-define3PosTumb("BATT_TEST_SW", 1, 13052, 1007, "Electric Panel", "Battery Test Switch")
+defineToggleSwitch("BATTERY_SW", 14, 13009, 1000, "Electric Panel", "Battery Switch")
+defineToggleSwitch("BATTERY_SW1", 14, 13010, 1000, "Electric Panel", "Battery Switch1")
+
+define3PosTumb("EXT_PW_SW", 14, 13047, 1001, "Electric Panel", "External Power/OFF/APU Switch")
+defineToggleSwitch("GEN1_SW", 14, 13048, 1003, "Electric Panel", "Generator 1 Switch")
+defineToggleSwitch("GEN2_SW", 14, 13049, 1004, "Electric Panel", "Generator 2 Switch")
+defineToggleSwitch("GEN3_SW", 14, 13050, 1005, "Electric Panel", "Generator 3 Switch")
+defineToggleSwitch("GEN4_SW", 14, 13051, 1006, "Electric Panel", "Generator 4 Switch")
+define3PosTumb("BATT_TEST_SW", 14, 13052, 1007, "Electric Panel", "Battery Test Switch")
 
 -- FIRE_HANDLES
 
 -- ENGINE_START_PANEL
-defineTumb("ENG1_START_SW", 1, 13053, 1009, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 1 Stop/Run/Start Selector")
-defineTumb("ENG2_START_SW", 1, 13054, 1012, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 2 Stop/Run/Start Selector")
-defineTumb("ENG3_START_SW", 1, 13055, 1015, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 3 Stop/Run/Start Selector")
-defineTumb("ENG4_START_SW", 1, 13056, 1018, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 4 Stop/Run/Start Selector")
+defineTumb("ENG1_START_SW", 14, 13053, 1009, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 1 Stop/Run/Start Selector")
+defineTumb("ENG2_START_SW", 14, 13054, 1012, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 2 Stop/Run/Start Selector")
+defineTumb("ENG3_START_SW", 14, 13055, 1015, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 3 Stop/Run/Start Selector")
+defineTumb("ENG4_START_SW", 14, 13056, 1018, 0.5, {-0.5, 1}, nil, false, "Engine Start Panel", "Engine 4 Stop/Run/Start Selector")
 
 -- APU_PANEL
-defineTumb("APU_START_SW", 1, 13056, 1050, 0.5, {0, 1}, nil, false, "APU Panel", "APU Stop/Run/Start Selector")
+defineTumb("APU_START_SW", 14, 13056, 1050, 0.5, {0, 1}, nil, false, "APU Panel", "APU Stop/Run/Start Selector")
 
 -- PROPELLER_CONTROL_PANEL
 
