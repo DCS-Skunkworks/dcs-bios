281f8d8116f99082ee94301c7c3539757b1cdd66
Sven <warlord12@freenet.de>
2021-06-16 23:17:58 +0200

    T-45 - Last state upload / HIND - first switches

--- Scripts/DCS-BIOS/lib/Mi-24P.lua
+++ Scripts/DCS-BIOS/lib/Mi-24P.lua
@@ -17,15 +17,62 @@ local defineToggleSwitch = BIOS.util.defineToggleSwitch
 local defineFixedStepTumb = BIOS.util.defineFixedStepTumb
 local defineString = BIOS.util.defineString
 local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
+local define3Pos2CommandSwitch = BIOS.util.define3Pos2CommandSwitch
 
+--Engine
+defineToggleSwitch("PLT_ROTOR_BRAKE", 3, 3011, 745, "Engine", "PILOT Rotor Brake")
+defineToggleSwitch("PLT_ENG_STOP_L", 3, 3009, 6, "Engine", "PILOT Left Engine Stop")
+defineToggleSwitch("PLT_ENG_STOP_R", 3, 3010, 7, "Engine", "PILOT Right Engine Stop")
+defineRotary("PLT_ENG_THROTTLE_L", 3, 3006, 5, "Engine", "PILOT Left Engine Throttle")
+defineRotary("PLT_ENG_THROTTLE_R", 3, 3008, 4, "Engine", "PILOT Right Engine Throttle")
+define3Pos2CommandSwitch("PLT_FREE_TURB_RPM", 3, 3020, 3019, 747, "Engine", "PILOT Readjust Free Turbine RPM, RIGHT/NEUTRAL/LEFT")
+
+--HydroInterface
+defineToggleSwitch("PLT_MAIN_AUXHYD_CV", 4, 3002, 216, "Hydro Interface", "PILOT Main/Auxiliary Hydraulic Switch Cover, UP/DOWN")
+defineToggleSwitch("PLT_MAIN_AUXHYD", 4, 3001, 217, "Hydro Interface", "PILOT Main/Auxiliary Hydraulic Switch, MAIN/AUXILIARY")
+defineToggleSwitch("PLT_MAIN_GEARHYD_CV", 4, 3004, 218, "Hydro Interface", "PILOT Main/Reserve Gear Hydraulic Switch Cover, UP/DOWN")
+defineToggleSwitch("PLT_MAIN_GEARHYD", 4, 3003, 219, "Hydro Interface", "PILOT Main/Reserve Gear Hydraulic Switch, MAIN/RESERVE")
+definePushButton("PLT_AUX_DISABLE", 4, 3005, 213, "Hydro Interface", "PILOT Auxiliary Disable Button")
+defineToggleSwitch("PLT_AUX_DISABLE_CV", 4, 3006, 215, "Hydro Interface", "PILOT Auxiliary Disable Button Cover, UP/DOWN")
+defineToggleSwitch("PLT_CONTROL_ASSUMEND_CV", 4, 3012, 47, "Hydro Interface", "PILOT Control Assumed Switch Cover, UP/DOWN")
+defineToggleSwitch("PLT_CONTROL_ASSUMEND", 4, 3011, 48, "Hydro Interface", "PILOT Control Assumed Switch Cover, ON/OFF")
+defineToggleSwitch("PLT_PEDAL_DAMPER_CV", 4, 3008, 289, "Hydro Interface", "PILOT Pedal Damper Cover, UP/DOWN")
+defineToggleSwitch("PLT_PEDAL_DAMPER", 4, 3007, 290, "Hydro Interface", "PILOT Pedal Damper, ON/OFF")
+defineToggleSwitch("OP_PEDAL_DAMPER_CV", 4, 3001, 666, "Hydro Interface", "OPERATOR Pedal Damper Cover, UP/DOWN")
+defineToggleSwitch("OP_PEDAL_DAMPER", 4, 3009, 667, "Hydro Interface", "OPERATOR Pedal Damper, ON/OFF")
+
+--Gauges
+defineFloat("OP_CANOPY_VALUE", 849, {0, 1}, "Gauges", "OPERATOR Canopy Position")
+defineFloat("PLT_CANOPY_PILOT_VALUE", 9, {0, 1}, "Gauges", "PILOT Door Position")
+defineFloat("PLT_VARIOMETER", 1, {-1, 1}, "Gauges", "PILOT Variometer")
+defineFloat("PLT_GMETER", 17, {-1, 1}, "Gauges", "PILOT G-Meter")
+defineFloat("PLT_GMETER_MIN", 16, {-1, 1}, "Gauges", "PILOT G-Meter Min")
+defineFloat("PLT_GMETER_MAX", 16, {-1, 1}, "Gauges", "PILOT G-Meter Max")
+defineFloat("PLT_IAS", 790, {-1, 1}, "Gauges", "PILOT IAS Speed")
+defineFloat("OP_IAS", 776, {-1, 1}, "Gauges", "OPERATOR IAS Speed")
+defineFloat("PLT_UKT2_PITCH", 949, {-1, 1}, "Gauges", "PILOT UKT-2 Pitch")
+defineFloat("PLT_UKT2_ROLL", 950, {1, -1}, "Gauges", "PILOT UKT-2 Roll")
+defineFloat("PLT_UKT2_FAIL_FLG", 849, {0, 1}, "Gauges", "PILOT UKT-2 Failure Flag")
 
 --Externals
 defineIntegerFromGetter("EXT_POSITION_LIGHTS", function()
-	if LoGetAircraftDrawArgumentValue(190) > 0 then return 1 else return 0 end
+	if LoGetAircraftDrawArgumentValue(191) > 0 then return 1 else return 0 end
 end, 1, "External Aircraft Model", "Position Lights")
-defineIntegerFromGetter("EXT_STROBE", function()
-	if LoGetAircraftDrawArgumentValue(193) > 0 then return 1 else return 0 end
-end, 1, "External Aircraft Model", "Strobe Light")
+defineIntegerFromGetter("EXT_NOSE_LIGHT", function()
+	if LoGetAircraftDrawArgumentValue(208) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Nose Light (yellow)")
+defineIntegerFromGetter("EXT_LAND_LIGHT", function()
+	if LoGetAircraftDrawArgumentValue(209) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Landing Light (yellow)")
+defineIntegerFromGetter("EXT_BEACON", function()
+	if LoGetAircraftDrawArgumentValue(83) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Beacon Light (red)")
+defineIntegerFromGetter("EXT_BLADE_L", function()
+	if LoGetAircraftDrawArgumentValue(47) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Blade Lights (yellow)")
+defineIntegerFromGetter("EXT_WIPER", function()
+	return math.floor(LoGetAircraftDrawArgumentValue(973)*65535)
+end, 65535, "External Aircraft Model", "Wiper")
 defineIntegerFromGetter("EXT_WOW_NOSE", function()
 	if LoGetAircraftDrawArgumentValue(1) > 0 then return 1 else return 0 end
 end, 1, "External Aircraft Model", "Weight ON Wheels Nose Gear")
--- Scripts/DCS-BIOS/lib/VNAO_T-45.lua
+++ Scripts/DCS-BIOS/lib/VNAO_T-45.lua
@@ -17,10 +17,68 @@ local definePotentiometer = BIOS.util.definePotentiometer
 local defineToggleSwitch = BIOS.util.defineToggleSwitch
 local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
 
+-------- FRONT Pit Clickables ------
+
 defineTumb("FLAPS_LVR", 19, 3013, 7, 0.5, {0, 1}, nil, false, "General", "Flaps Lever, UP/ 1/2 /DOWN")
 defineToggleSwitch("EMERG_FLAP_SW", 19, 3027, 184, "General", "EMER FLAPS Switch, NORM/DOWN")
 defineToggleSwitch("PARK_BRAKE", 19, 3038, 117, "General", "Parking Brake Handle, OUT/IN")
 definePushButton("HYD2_RESET_BTN", 19, 3025, 66, "Gear", "HYD 2 Reset Button")
+
+-- MFD Left ----
+definePushButton("MFD_L_ON_DAY", 12, 3501, 424, "MFD Left", "Left MFD On Day")
+definePushButton("MFD_L_ON_NGT", 12, 3502, 425, "MFD Left", "Left MFD On Night")
+definePushButton("MFD_L_OFF", 12, 3503, 426, "MFD Left", "Left MFD Off")
+definePushButton("MFD_L_BRGHT_INC", 12, 3504, 422, "MFD Left", "Left MFD Brightness Increase")
+definePushButton("MFD_L_BRGHT_DEC", 12, 3505, 423, "MFD Left", "Left MFD Brightness Decrease")
+definePushButton("MFD_L_BTN_20", 12, 3525, 34, "MFD Left", "Left MFD Push Button 20")
+definePushButton("MFD_L_BTN_19", 12, 3524, 33, "MFD Left", "Left MFD Push Button 19")
+definePushButton("MFD_L_BTN_18", 12, 3523, 32, "MFD Left", "Left MFD Push Button 18")
+definePushButton("MFD_L_BTN_17", 12, 3522, 31, "MFD Left", "Left MFD Push Button 17")
+definePushButton("MFD_L_BTN_16", 12, 3521, 30, "MFD Left", "Left MFD Push Button 16")
+definePushButton("MFD_L_BTN_15", 12, 3520, 29, "MFD Left", "Left MFD Push Button 15")
+definePushButton("MFD_L_BTN_14", 12, 3519, 28, "MFD Left", "Left MFD Push Button 14")
+definePushButton("MFD_L_BTN_13", 12, 3518, 27, "MFD Left", "Left MFD Push Button 13")
+definePushButton("MFD_L_BTN_12", 12, 3517, 26, "MFD Left", "Left MFD Push Button 12")
+definePushButton("MFD_L_BTN_11", 12, 3516, 25, "MFD Left", "Left MFD Push Button 11")
+definePushButton("MFD_L_BTN_10", 12, 3515, 24, "MFD Left", "Left MFD Push Button 10")
+definePushButton("MFD_L_BTN_9", 12, 3514, 23, "MFD Left", "Left MFD Push Button 9")
+definePushButton("MFD_L_BTN_8", 12, 3513, 22, "MFD Left", "Left MFD Push Button 8")
+definePushButton("MFD_L_BTN_7", 12, 3512, 21, "MFD Left", "Left MFD Push Button 7")
+definePushButton("MFD_L_BTN_6", 12, 3511, 20, "MFD Left", "Left MFD Push Button 6")
+definePushButton("MFD_L_BTN_5", 12, 3510, 19, "MFD Left", "Left MFD Push Button 5")
+definePushButton("MFD_L_BTN_4", 12, 3509, 18, "MFD Left", "Left MFD Push Button 4")
+definePushButton("MFD_L_BTN_3", 12, 3508, 17, "MFD Left", "Left MFD Push Button 3")
+definePushButton("MFD_L_BTN_2", 12, 3507, 16, "MFD Left", "Left MFD Push Button 2")
+definePushButton("MFD_L_BTN_1", 12, 3506, 15, "MFD Left", "Left MFD Push Button 1")
+
+-- MFD Right ----
+definePushButton("MFD_R_ON_DAY", 12, 3501, 434, "MFD Right", "Right MFD On Day")
+definePushButton("MFD_R_ON_NGT", 12, 3502, 435, "MFD Right", "Right MFD On Night")
+definePushButton("MFD_R_OFF", 12, 3503, 436, "MFD Right", "Right MFD Off")
+definePushButton("MFD_R_BRGHT_INC", 12, 3504, 432, "MFD Right", "Right MFD Brightness Increase")
+definePushButton("MFD_R_BRGHT_DEC", 12, 3505, 433, "MFD Right", "Right MFD Brightness Decrease")
+definePushButton("MFD_R_BTN_20", 12, 3525, 59, "MFD Right", "Right MFD Push Button 20")
+definePushButton("MFD_R_BTN_19", 12, 3524, 58, "MFD Right", "Right MFD Push Button 19")
+definePushButton("MFD_R_BTN_18", 12, 3523, 57, "MFD Right", "Right MFD Push Button 18")
+definePushButton("MFD_R_BTN_17", 12, 3522, 56, "MFD Right", "Right MFD Push Button 17")
+definePushButton("MFD_R_BTN_16", 12, 3521, 55, "MFD Right", "Right MFD Push Button 16")
+definePushButton("MFD_R_BTN_15", 12, 3520, 54, "MFD Right", "Right MFD Push Button 15")
+definePushButton("MFD_R_BTN_14", 12, 3519, 53, "MFD Right", "Right MFD Push Button 14")
+definePushButton("MFD_R_BTN_13", 12, 3518, 52, "MFD Right", "Right MFD Push Button 13")
+definePushButton("MFD_R_BTN_12", 12, 3517, 51, "MFD Right", "Right MFD Push Button 12")
+definePushButton("MFD_R_BTN_11", 12, 3516, 50, "MFD Right", "Right MFD Push Button 11")
+definePushButton("MFD_R_BTN_10", 12, 3515, 49, "MFD Right", "Right MFD Push Button 10")
+definePushButton("MFD_R_BTN_9", 12, 3514, 48, "MFD Right", "Right MFD Push Button 9")
+definePushButton("MFD_R_BTN_8", 12, 3513, 47, "MFD Right", "Right MFD Push Button 8")
+definePushButton("MFD_R_BTN_7", 12, 3512, 46, "MFD Right", "Right MFD Push Button 7")
+definePushButton("MFD_R_BTN_6", 12, 3511, 45, "MFD Right", "Right MFD Push Button 6")
+definePushButton("MFD_R_BTN_5", 12, 3510, 44, "MFD Right", "Right MFD Push Button 5")
+definePushButton("MFD_R_BTN_4", 12, 3509, 43, "MFD Right", "Right MFD Push Button 4")
+definePushButton("MFD_R_BTN_3", 12, 3508, 42, "MFD Right", "Right MFD Push Button 3")
+definePushButton("MFD_R_BTN_2", 12, 3507, 41, "MFD Right", "Right MFD Push Button 2")
+definePushButton("MFD_R_BTN_1", 12, 3506, 40, "MFD Right", "Right MFD Push Button 1")
+
+-------- REAR Pit Clickables ------
 
 --Warning, Caution and IndicatorLights
 defineIndicatorLight("L_AOA_SLOW", 320, "Warning, Caution and IndicatorLights","AOA Light Slow (green)")
