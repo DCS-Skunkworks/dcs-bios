950ad1fb3cf7cfea8b0a512fcc018da0ab3b90be
Sven <warlord12@freenet.de>
2021-07-19 17:26:26 +0200

    Add Support for Extra 330SR (uses Edge540.lua)

--- Scripts/DCS-BIOS/lib/Edge540.lua
+++ Scripts/DCS-BIOS/lib/Edge540.lua
@@ -1,5 +1,5 @@
 BIOS.protocol.beginModule("Edge540", 0x5600)
-BIOS.protocol.setExportModuleAircrafts({"Edge540"})
+BIOS.protocol.setExportModuleAircrafts({"Edge540", "Extra330SR"})
 --by WarLord (aka BlackLibrary)
 
 local documentation = moduleBeingDefined.documentation
@@ -34,8 +34,8 @@ definePushButton("PAGE_3BTN_MP3", 8, 3018, 31, "Main Display", "Display Button 3
 definePushButton("PAGE_4BTN_CONF", 8, 3001, 30, "Main Display", "Display Button 4 Configuration Page")
 
 --System Page Buttons
-definePushButton("SYS_NAV_BTN", 4, 3001, 0, "System Page", "System Page - Navigation Lights")
-definePushButton("SYS_FLOOD_BTN", 4, 3012, 0, "System Page", "System Page - Flood Lights")
+definePushButton("SYS_NAV_BTN", 4, 3001, 61, "System Page", "System Page - Navigation Lights")
+definePushButton("SYS_FLOOD_BTN", 4, 3012, 63, "System Page", "System Page - Flood Lights")
 definePushButton("SYS_GAUGE_BTN", 4, 3013, 42, "System Page", "System Page - Gauge Lights")
 definePushButton("SYS_WALL_BTN", 8, 3019, 0, "System Page", "System Page - Wallpaper")
 defineTumb("SYS_FLOOD_L_COL", 8, 3023, 0, 0.1, {0.0, 0.5}, nil, false, "System Page", "System Page - Flood Light Color")
@@ -46,10 +46,11 @@ definePushButton("MP3_NEXT", 9, 3031, 0, "Mp3 Page", "Mp3 Next Track")
 definePushButton("MP3_PREV", 9, 3032, 0, "Mp3 Page", "Mp3 Previous Track")
 
 --Configuration Page Buttons
-defineToggleSwitch("CONF_CUSTOM_1_BTN", 8, 3010, 0, "Config Page", "Config Page - Attach/Remove Winglets")
-defineToggleSwitch("CONF_CUSTOM_2_BTN", 8, 3011, 0, "Config Page", "Config Page - Attach/Remove Spades")
-defineToggleSwitch("CONF_CUSTOM_3_BTN", 8, 3012, 0, "Config Page", "Config Page - Attach/Remove Lights")
-defineTumb("CONF_PILOT_SEL", 8, 3013, 0, 0.1, {0.0, 0.6}, nil, false, "Config Page", "Config Page - Select Pilot")
+defineToggleSwitch("CONF_CUSTOM_1_BTN", 8, 3010, 70, "Config Page", "Config Page - Attach/Remove Winglets")
+defineToggleSwitch("CONF_CUSTOM_2_BTN", 8, 3011, 71, "Config Page", "Config Page - Attach/Remove Spades")
+defineToggleSwitch("CONF_CUSTOM_2_BTN_330", 8, 3011, 73, "Config Page", "Config Page - Attach/Remove Spades Extra330SR")
+defineToggleSwitch("CONF_CUSTOM_3_BTN", 8, 3012, 720, "Config Page", "Config Page - Attach/Remove Lights")
+defineTumb("CONF_PILOT_SEL", 8, 3013, 6, 0.1, {0.0, 0.6}, nil, false, "Config Page", "Config Page - Select Pilot")
 definePushButton("CONF_CUSTOM_5_BTN", 2, 3006, 0, "Config Page", "Config Page - Hide Aersti Chart")
 
 defineToggleSwitch("SMK_ARM", 1, 3025, 27, "Smoke", "Smoke Arm")
