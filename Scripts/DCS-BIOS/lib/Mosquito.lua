6801f9d322f9cb9d87126b7c04429eecb8297eae
Sven <warlord12@freenet.de>
2021-08-13 20:47:59 +0200

    Add files via upload

---
+++ Scripts/DCS-BIOS/lib/Mosquito.lua
@@ -0,0 +1,43 @@
+BIOS.protocol.beginModule("Mosquito", 0x7000)
+BIOS.protocol.setExportModuleAircrafts({"Mosquito"})
+
+local documentation = moduleBeingDefined.documentation
+
+local document = BIOS.util.document  
+
+local parse_indication = BIOS.util.parse_indication
+
+local defineFloat = BIOS.util.defineFloat
+local defineIndicatorLight = BIOS.util.defineIndicatorLight
+local definePushButton = BIOS.util.definePushButton
+local definePotentiometer = BIOS.util.definePotentiometer
+local defineRotary = BIOS.util.defineRotary
+local defineTumb = BIOS.util.defineTumb
+local defineToggleSwitch = BIOS.util.defineToggleSwitch
+local defineString = BIOS.util.defineString
+local defineIntegerFromGetter = BIOS.util.defineIntegerFromGetter
+
+-- remove Arg# Pilot 540
+
+--Externals
+defineIntegerFromGetter("EXT_POSITION_LIGHT_LEFT", function()
+	if LoGetAircraftDrawArgumentValue(190) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Left Position Light (red)")
+defineIntegerFromGetter("EXT_POSITION_LIGHT_RIGHT", function()
+	if LoGetAircraftDrawArgumentValue(191) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Right Position Light (green)")
+defineIntegerFromGetter("EXT_POSITION_LIGHT_TAIL", function()
+	if LoGetAircraftDrawArgumentValue(192) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Tail Position Light (white)")
+
+defineIntegerFromGetter("EXT_WOW_NOSE", function()
+	if LoGetAircraftDrawArgumentValue(1) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Weight ON Wheels Nose Gear")
+defineIntegerFromGetter("EXT_WOW_RIGHT", function()
+	if LoGetAircraftDrawArgumentValue(4) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Weight ON Wheels Right Gear")
+defineIntegerFromGetter("EXT_WOW_LEFT", function()
+	if LoGetAircraftDrawArgumentValue(6) > 0 then return 1 else return 0 end
+end, 1, "External Aircraft Model", "Weight ON Wheels Left Gear")
+
+BIOS.protocol.endModule()
