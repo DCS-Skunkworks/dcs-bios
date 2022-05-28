64c4264d0458916e2e1b9e32349e3f0a291ddedb
Sven <warlord12@freenet.de>
2022-05-28 12:53:08 +0200

    merge dev build

--- Scripts/DCS-BIOS/lib/SuperCarrier.lua
+++ Scripts/DCS-BIOS/lib/SuperCarrier.lua
@@ -10,9 +10,9 @@ local parse_indication = BIOS.util.parse_indication
 local definePushButton = BIOS.util.definePushButton
 local defineIndicatorLight = BIOS.util.defineIndicatorLight
 
-definePushButton("CVN_CUT_L", 0, 3002, 27, "Panel", "Cut Lights")
-definePushButton("CVN_WAVEOFF_L", 0, 3003, 28, "Panel", "WaveOff Lights")
-definePushButton("CVN_DECK_L", 0, 3004, 34, "Panel", "DECK Lights On")
+definePushButton("CVN_CUT_L", 0, 2, 27, "Panel", "Cut Lights")
+definePushButton("CVN_WAVEOFF_L", 0, 3, 28, "Panel", "WaveOff Lights")
+definePushButton("CVN_DECK_L", 0, 4, 34, "Panel", "DECK Lights On")
 
 defineIndicatorLight("CVN_MASTER_IND", 10, "Lights", "Master Caution Light (yellow)")
 defineIndicatorLight("CVN_CUT_IND", 11, "Lights", "CUT Light (green)")
