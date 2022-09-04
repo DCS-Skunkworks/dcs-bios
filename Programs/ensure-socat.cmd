0566b36606605b17587bae2f0e027e75057f9a9c
Talbot McInnis <talbotmcinnis@alumni.uwo.ca>
2022-09-03 20:44:45 -0400

    Auto-unpack socat 64 when not found

---
+++ Programs/ensure-socat.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM This serves to help new users/installer unpack socat correctly.
+
+if not EXIST socat\socat.exe (
+	echo Socat not found.  Extracting...
+	tar -xf ./socat/WIN_socat-1.7.4.3-64bit.zip
+)
+
+if NOT EXIST socat\socat.exe CANCEL
