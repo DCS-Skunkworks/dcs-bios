4e019c316c27fcfd81d1ebf7ba343c1d69b16b3f
Sven <warlord12@freenet.de>
2022-11-23 22:38:55 +0100

    Update for 1.7.4.4

--- Programs/ensure-socat.cmd
+++ Programs/ensure-socat.cmd
@@ -3,7 +3,7 @@ REM This serves to help new users/installer unpack socat correctly.
 
 if not EXIST socat\socat.exe (
 	echo Socat not found.  Extracting...
-	tar -xf ./socat/WIN_socat-1.7.4.3-64bit.zip
+	tar -xf ./socat/WIN_socat-1.7.4.4-64bit.zip
 )
 
 if NOT EXIST socat\socat.exe CANCEL
