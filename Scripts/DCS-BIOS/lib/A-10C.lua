e7a9f9f5075d06e5ae5807c3ce1da21a58b65244
Sven <warlord12@freenet.de>
2023-06-24 07:52:13 +0200

    new V Nr after PR

--- Scripts/DCS-BIOS/lib/A-10C.lua
+++ Scripts/DCS-BIOS/lib/A-10C.lua
@@ -1,6 +1,6 @@
 BIOS.protocol.beginModule("A-10C", 0x1000)
 BIOS.protocol.setExportModuleAircrafts({"A-10C", "A-10C_2"})
---overhaul by WarLord,charliefoxtwo,talbotmcinnis&DeadMeat v2.9
+--overhaul by WarLord,charliefoxtwo,talbotmcinnis&DeadMeat v2.9a
 local inputProcessors = moduleBeingDefined.inputProcessors
 local documentation = moduleBeingDefined.documentation
 
