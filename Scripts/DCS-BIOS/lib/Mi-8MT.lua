c9f3dae9c82e634924bb14cd0da1f7745604b7e4
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-09-11 13:44:52 +0300

    Frequency parts concatenated to satisfy lua syntax inspection

--- Scripts/DCS-BIOS/lib/Mi-8MT.lua
+++ Scripts/DCS-BIOS/lib/Mi-8MT.lua
@@ -505,6 +505,7 @@ defineFixedStepTumb("R863_FREQ2", 38, 3007, 158, 0.1, {0, 1}, {-0.1, 0.1}, nil, 
 defineFixedStepTumb("R863_FREQ3", 38, 3008, 159, 0.1, {0, 1}, {-0.1, 0.1}, nil, "R-863", "R-863, 100KHz Rotary Knob")
 defineFixedStepTumb("R863_FREQ4", 38, 3009, 160, 0.25, {0, 1}, {-0.1, 0.1}, nil, "R-863", "R-863, 1KHz Rotary Knob")
 
+local asdasd = 0
 local function getR863Frequency()
     local freq1 = R863_FREQ1_POS[string.format("%.0f", GetDevice(0):get_argument_value(157)*100)]
 	if freq1 == nil then freq1 = "10" end
@@ -524,8 +525,10 @@ local function getR863Frequency()
 	elseif freq4F >= 62.5 and freq4F < 87.5 then freq4 = "75"
 	elseif freq4F >= 87.5                   then freq4 = "00"
 	end
+    
+    local frequency = freq1 .. freq2 .. "." .. freq3 .. freq4
 
-	return  freq1 .. freq2 .. "." .. freq3 .. freq4 or "100.000"
+	return  frequency or "100.000"
 end
 
 defineTumb("R828_PRST_CHAN_SEL", 39, 3001, 735, 0.1, {0, 0.9}, nil, false, "R-828", "R-828, Radio Channel Selector Knob")
