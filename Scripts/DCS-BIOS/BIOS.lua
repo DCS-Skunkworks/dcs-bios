629db4e5c00706f695efe0d4c2c5dfad5aa1fad3
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2023-09-28 19:41:42 -0700

    Fix file name case-sensitivity issue

--- Scripts/DCS-BIOS/BIOS.lua
+++ Scripts/DCS-BIOS/BIOS.lua
@@ -88,11 +88,11 @@ dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/MB-339.lua]]) -- ID = 28, ProperNa
 dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/Mi-8MT.lua]]) -- ID = 29, ProperName = Mi-8MT
 dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/Mi-24P.lua]]) -- ID = 42, ProperName = Mi-24P HIND
 dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/MirageF1.lua]]) -- ID = 13, ProperName = Mirage F1
--- dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/Mig-15bis.lua]]) -- ID = 30, ProperName = MiG-15bis
+-- dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/MiG-15bis.lua]]) -- ID = 30, ProperName = MiG-15bis
 local MiG_15bis = require "MiG-15bis"
 BIOS.protocol.writeNewModule(MiG_15bis)
-dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/Mig-19P.lua]]) -- ID = 31, ProperName = MiG-19P Farmer
-dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/Mig-21Bis.lua]]) -- ID = 32, ProperName = MiG-21bis
+dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/MiG-19P.lua]]) -- ID = 31, ProperName = MiG-19P Farmer
+dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/MiG-21Bis.lua]]) -- ID = 32, ProperName = MiG-21bis
 dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/Mosquito.lua]]) -- ID = 45, ProperName = Mosquito FB Mk. VI
 dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/NS430.lua]]) -- ID = 33, ProperName = NS 430 GPS
 dofile(lfs.writedir()..[[Scripts/DCS-BIOS/lib/P-47D.lua]]) -- ID = 34, ProperName = P-47D Thunderbolt
