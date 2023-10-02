37276dfcdcafdcbc047996a3900417fb7ae18ac9
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-02 18:08:53 +0300

    Update Functions.lua

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -10,17 +10,35 @@ function Functions.ScriptPath()
     return str:match("(.*/)")
 end
 
+ ---@func Returns value if not nil or an empty string
+ ---@param value string
+ ---@return string
+ function Functions.Coerce_nil_to_string(value)
+	if value == nil then
+		return ""
+	else
+		return value
+	end
+end
 
  --Constructs a string of the specified length with the left padded by whitespace if necessary.
 ---@param str string The base text
 ---@param len number The length the string should be
 ---@return string result A new string of length len, with whitespace padding added to the left as necessary
 function Functions.PadLeft(str, len)
-	str = tostring(coerce_nil_to_string(str))
+	str = tostring(Functions.Coerce_nil_to_string(str))
     return string.rep(' ', len - #str)..str
 end
 
+
 return Functions
+
+
+
+
+
+
+
 
 
 
