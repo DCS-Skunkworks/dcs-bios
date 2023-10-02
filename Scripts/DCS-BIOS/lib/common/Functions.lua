7358b412566857b67c390885adb55f29449ed505
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-02 18:01:49 +0300

    Make module of Functions

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -1,13 +1,26 @@
+module("Functions", package.seeall)
 
----@func Returns the path of the calling script
+local Functions = {}
+
+
+---Returns the path of the calling script
 ---@return string
-function ScriptPath()
+function Functions.ScriptPath()
     local str = debug.getinfo(2, "S").source:sub(2)
     return str:match("(.*/)")
- end
+end
 
 
+ --Constructs a string of the specified length with the left padded by whitespace if necessary.
+---@param str string The base text
+---@param len number The length the string should be
+---@return string result A new string of length len, with whitespace padding added to the left as necessary
+function Functions.PadLeft(str, len)
+	str = tostring(coerce_nil_to_string(str))
+    return string.rep(' ', len - #str)..str
+end
 
+return Functions
 
 
 
