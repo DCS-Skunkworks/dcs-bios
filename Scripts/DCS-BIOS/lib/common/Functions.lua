9bf25f9bed652d521812c60e8dc537f006385e61
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-02 18:05:27 +0300

    Update Functions.lua

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -6,7 +6,16 @@ function ScriptPath()
     return str:match("(.*/)")
  end
 
-
+ ---@func Returns value if not nil or an empty string
+ ---@param value string
+ ---@return string
+ function Coerce_nil_to_string(value)
+	if value == nil then
+		return ""
+	else
+		return value
+	end
+end
 
 
 
