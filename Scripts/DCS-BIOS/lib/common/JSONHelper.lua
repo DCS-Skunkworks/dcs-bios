59ff59af37b6c4c48c30b6edbe3a9d81fd36b66d
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-03 09:22:45 +0300

    stylua added, add local require of JSON

--- Scripts/DCS-BIOS/lib/common/JSONHelper.lua
+++ Scripts/DCS-BIOS/lib/common/JSONHelper.lua
@@ -1,5 +1,8 @@
 module("JSONHelper", package.seeall)
 
+local json = loadfile([[Scripts/JSON.lua]]) -- try to load json from dcs
+local JSON = json and json() or require("JSON") -- if that fails, fall back to module that we can define
+
 --- @class JSONHelper
 local JSONHelper = {}
 
@@ -7,11 +10,10 @@ local JSONHelper = {}
 --- @param value any
 --- @param filename string File name including path
 function JSONHelper.encode_to_file(value, filename)
-	local JSON = BIOS.json
 	local file, err = io.open(filename, "w")
 
-	if(err ~= nil)then
-		error("Failed to open file "..filename.." for writing => "..err, 1)
+	if err ~= nil then
+		error("Failed to open file " .. filename .. " for writing => " .. err, 1)
 	end
 
 	local json_string = JSON:encode_pretty(value)
@@ -24,24 +26,24 @@ end
 
 --- @func Encodes to JSON and writes to file
 --- @param filename string File name including path
---- @return any
+--- @return table
 function JSONHelper.decode_from_file(filename)
-	local JSON = BIOS.json
-	local json_string
+	local json_table
 
 	local file, err = io.open(filename, "r")
 
-	if(err ~= nil)then
-		error("Failed to open file "..filename.." for reading. => "..err, 1)
+	if err ~= nil then
+		error("Failed to open file " .. filename .. " for reading. => " .. err, 1)
 	end
 
-	if(file ~= nil) then
-		json_string = JSON:decode(file:read("*a"))
+	if file ~= nil then
+		--- @type table
+		json_table = JSON:decode(file:read("*a"))
 		file:close()
 		file = nil
 	end
 
-	return json_string
+	return json_table
 end
 
 return JSONHelper
