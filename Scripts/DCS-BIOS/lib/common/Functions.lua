35e33527db7d9e9936af0c1ff2efff5abd3972b7
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-03 09:52:32 +0300

    Update Functions.lua

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -2,18 +2,17 @@ module("Functions", package.seeall)
 
 local Functions = {}
 
-
 ---Returns the path of the calling script
 ---@return string
 function Functions.ScriptPath()
-    local str = debug.getinfo(2, "S").source:sub(2)
-    return str:match("(.*/)")
+	local str = debug.getinfo(2, "S").source:sub(2)
+	return str:match("(.*/)")
 end
 
- ---@func Returns value if not nil or an empty string
- ---@param value string
- ---@return string
- function Functions.Coerce_nil_to_string(value)
+---@func Returns value if not nil or an empty string
+---@param value string
+---@return string
+function Functions.Coerce_nil_to_string(value)
 	if value == nil then
 		return ""
 	else
@@ -21,14 +20,13 @@ end
 	end
 end
 
- --Constructs a string of the specified length with the left padded by whitespace if necessary.
+--Constructs a string of the specified length with the left padded by whitespace if necessary.
 ---@param str string The base text
 ---@param len number The length the string should be
 ---@return string result A new string of length len, with whitespace padding added to the left as necessary
 function Functions.PadLeft(str, len)
 	str = tostring(Functions.Coerce_nil_to_string(str))
-    return string.rep(' ', len - #str)..str
+	return string.rep(" ", len - #str) .. str
 end
-
 
 return Functions
