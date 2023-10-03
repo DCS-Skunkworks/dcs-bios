ac28c4dea67fb5eec0ff25605488b3266988b30e
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-03 09:45:01 +0300

    Create JSONHelper.lua (#325)
    * Create JSONHelper.lua
    
    * stylua added, add local require of JSON
    
    * stylua common folder

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -1,13 +1,6 @@
-
 ---@func Returns the path of the calling script
 ---@return string
 function ScriptPath()
-    local str = debug.getinfo(2, "S").source:sub(2)
-    return str:match("(.*/)")
- end
-
-
-
-
-
-
+	local str = debug.getinfo(2, "S").source:sub(2)
+	return str:match("(.*/)")
+end
---
+++ Scripts/DCS-BIOS/lib/common/JSONHelper.lua
@@ -0,0 +1,49 @@
+module("JSONHelper", package.seeall)
+
+local json = loadfile([[Scripts/JSON.lua]]) -- try to load json from dcs
+local JSON = json and json() or require("JSON") -- if that fails, fall back to module that we can define
+
+--- @class JSONHelper
+local JSONHelper = {}
+
+--- @func Encodes to JSON and writes to file
+--- @param value any
+--- @param filename string File name including path
+function JSONHelper.encode_to_file(value, filename)
+	local file, err = io.open(filename, "w")
+
+	if err ~= nil then
+		error("Failed to open file " .. filename .. " for writing => " .. err, 1)
+	end
+
+	local json_string = JSON:encode_pretty(value)
+	if file then
+		file:write(json_string)
+		file:close()
+		file = nil
+	end
+end
+
+--- @func Encodes to JSON and writes to file
+--- @param filename string File name including path
+--- @return table
+function JSONHelper.decode_from_file(filename)
+	local json_table
+
+	local file, err = io.open(filename, "r")
+
+	if err ~= nil then
+		error("Failed to open file " .. filename .. " for reading. => " .. err, 1)
+	end
+
+	if file ~= nil then
+		--- @type table
+		json_table = JSON:decode(file:read("*a"))
+		file:close()
+		file = nil
+	end
+
+	return json_table
+end
+
+return JSONHelper
