e956dddacc496935c720607a73f7fd0a3d50c369
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-03 10:06:34 +0300

    Function module (#324)
    * Make module of Functions
    
    * Update Functions.lua
    
    * Update Functions.lua
    
    * lowercase function names
    
    * truthy falsy
    
    * Update Functions.lua

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -1,6 +1,28 @@
----@func Returns the path of the calling script
+module("Functions", package.seeall)
+
+local Functions = {}
+
+---Returns the path of the calling script
 ---@return string
-function ScriptPath()
+function Functions.scriptPath()
 	local str = debug.getinfo(2, "S").source:sub(2)
 	return str:match("(.*/)")
 end
+
+---@func Returns value if not nil or an empty string
+---@param value string
+---@return string
+function Functions.coerce_nil_to_string(value)
+	return value and value or ""
+end
+
+--Constructs a string of the specified length with the left padded by whitespace if necessary.
+---@param str string The base text
+---@param len number The length the string should be
+---@return string | nil result A new string of length len, with whitespace padding added to the left as necessary
+function Functions.padLeft(str, len)
+	str = tostring(Functions.coerce_nil_to_string(str))
+	return string.rep(" ", len - #str) .. str
+end
+
+return Functions
