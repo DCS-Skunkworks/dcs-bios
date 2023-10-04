653444825d04ef5e908410e99d184bf68061d81e
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2023-10-03 21:25:45 -0700

    Correct documentation in Functions

--- Scripts/DCS-BIOS/lib/common/Functions.lua
+++ Scripts/DCS-BIOS/lib/common/Functions.lua
@@ -10,16 +10,16 @@ function Functions.scriptPath()
 end
 
 --- @func Returns value if not nil or an empty string
---- @param value string
+--- @param value string?
 --- @return string
 function Functions.coerce_nil_to_string(value)
 	return value and value or ""
 end
 
 --- @func Constructs a string of the specified length with the left padded by whitespace if necessary.
---- @param str string The base text
+--- @param str string? The base text
 --- @param len number The length the string should be
---- @return string | nil result A new string of length len, with whitespace padding added to the left as necessary
+--- @return string result A new string of length len, with whitespace padding added to the left as necessary
 function Functions.padLeft(str, len)
 	str = tostring(Functions.coerce_nil_to_string(str))
 	return string.rep(" ", len - #str) .. str
