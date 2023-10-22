d5ed3f899c0007a72c9771b9d06a7271aebb3e78
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2023-10-22 13:33:26 -0700

    Remove LoGetModelTime from BIOSStateMachine constructor
    It isn't actually needed there, and removing it reduces dependency on globals

--- Scripts/DCS-BIOS/test/compile/LocalCompile.lua
+++ Scripts/DCS-BIOS/test/compile/LocalCompile.lua
@@ -8,12 +8,6 @@ package.path = "./Scripts/DCS-BIOS/test/compile/?.lua;" .. package.path
 package.path = "./Scripts/DCS-BIOS/test/ext/?.lua;" .. package.path
 package.path = "./Scripts/DCS-BIOS/test/io/?.lua;" .. package.path
 
---- Returns the simulation time
---- @return number
-function LoGetModelTime()
-	return 0
-end
-
 lfs = require("lfs")
 
 -- Include these that will mock the DCS APIs and the socket.
