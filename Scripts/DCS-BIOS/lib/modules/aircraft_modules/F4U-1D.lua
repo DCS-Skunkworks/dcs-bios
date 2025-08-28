6efe980207bc62e4a26e901df22539bded93a16c
Blackhawkops <jcburford12@gmail.com>
2025-08-27 21:37:02 -0600

    F4U-1D: Add Directional Gyro (#1230)
    * F4U-1D: Add Directional Gyro
    
    Fixes #1167
    
    * Update Scripts/DCS-BIOS/lib/modules/aircraft_modules/F4U-1D.lua
    
    Co-authored-by: Charlie <30303272+charliefoxtwo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Charlie <30303272+charliefoxtwo@users.noreply.github.com>

--- Scripts/DCS-BIOS/lib/modules/aircraft_modules/F4U-1D.lua
+++ Scripts/DCS-BIOS/lib/modules/aircraft_modules/F4U-1D.lua
@@ -174,7 +174,12 @@ F4U_1D:defineFloat("TACHOMETER_DIAL", 22, { 0, 1 }, TACHOMETER, "Tachometer Gaug
 -- local ALTIMETER = "Altimeter"
 
 -- Directional Gyro
--- local DIRECTIONAL_GYRO = "Directional Gyro"
+local DIRECTIONAL_GYRO = "Directional Gyro"
+
+F4U_1D:defineFloat("DIRECTIONAL_GYRO_HEADING", 12, { 0, 1 }, DIRECTIONAL_GYRO, "Directional Gyro Heading")
+F4U_1D:defineFloat("DIRECTIONAL_GYRO_HEADING_CAGE_FLAG", 313, { 0, 1 }, DIRECTIONAL_GYRO, "Directional Gyro Heading Cage Flag")
+F4U_1D:definePotentiometer("DIRECTIONAL_GYRO_SET", devices.NAV, 3362, 13, { -1, 1 }, DIRECTIONAL_GYRO, "Directional Gyro Heading Set")
+F4U_1D:definePushButton("DIRECTIONAL_GYRO_CAGE", devices.NAV, 3365, 313, DIRECTIONAL_GYRO, "Directional Gyro Heading Cage")
 
 -- Manifold Pressure Gauge
 local MANIFOLD_PRESSURE = "Manifold Pressure Gauge"
