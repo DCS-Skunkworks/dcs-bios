f1865f68e4d7eb748b0c7f37d19b214de62ac13f
Elliot Thiebaut <hello@elliotthiebaut.com>
2025-11-11 21:27:14 +0100

    MiG-29A: Add Attitude director indicator (#1389)
    * Add Attitude director indicator
    
    * Add backs missing category
    
    * Removed duplicate category

--- Scripts/DCS-BIOS/lib/modules/aircraft_modules/MiG-29A.lua
+++ Scripts/DCS-BIOS/lib/modules/aircraft_modules/MiG-29A.lua
@@ -433,11 +433,24 @@ MiG_29A:defineFloat("IP_52_03_LEFT_GEAR_INDICATOR", 39, { 0, 1 }, IP_52_03, "Lef
 MiG_29A:defineFloat("IP_52_03_RIGHT_GEAR_INDICATOR", 41, { 0, 1 }, IP_52_03, "Right Gear Indicator")
 MiG_29A:defineFloat("IP_52_03_GEAR_WARNING_INDICATOR", 46, { 0, 1 }, IP_52_03, "Gear Release Warning Indicator")
 
--- ADI
-
 -- HSI
 
--- Combined indicator (VVI/Turn/Slip indicator)
+-- DA-200P Combined indicator (VVI/Turn/Slip indicator)
+
+-- ADI
+local ADI = "ADI"
+
+MiG_29A:defineFloat("ADI_PITCH_INDICATOR", 81, { -1, 1 }, ADI, "Pitch Indicator")
+MiG_29A:defineFloat("ADI_ROLL_INDICATOR", 30, { -1, 1 }, ADI, "Roll Indicator")
+MiG_29A:defineFloat("ADI_SLIP_INDICATOR", 31, { -1, 1 }, ADI, "Slip Indicator")
+MiG_29A:defineFloat("ADI_HORIZ_FLIGHT_DIRECTOR", 27, { -1, 1 }, ADI, "Horizontal Flight Director")
+MiG_29A:defineFloat("ADI_PITCH_OFF_INDICATOR", 94, { 0, 1 }, ADI, "Pitch Off Indicator")
+MiG_29A:defineFloat("ADI_AZIMUTH_OFF_INDICATOR", 96, { 0, 1 }, ADI, "Azimuth Off Indicator")
+MiG_29A:defineFloat("ADI_HORIZ_PLANE_INDICATOR", 393, { -1, 1 }, ADI, "Horizontal Plane Indicator")
+MiG_29A:defineFloat("ADI_VERT_PLANE_INDICATOR", 392, { -1, 1 }, ADI, "Vertical Plane Indicator")
+MiG_29A:definePotentiometer("ADI_POSITION_ADJUSTMENT_KNOB", devices.ADI, 3001, 264, { -1, 1 }, ADI, "Aircraft Symbol Adjustment Knob")
+MiG_29A:definePushButton("ADI_GYRO_CAGE_BUTTON", devices.ADI, 3002, 265, ADI, "Gyro Cage Button")
+MiG_29A:defineFloat("ADI_GYRO_CAGE_LIGHT", 262, { 0, 1 }, ADI, "Gyro Cage Light")
 
 -- TAS indicator
 
